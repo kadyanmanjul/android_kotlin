78ee4fe9ec814ebd39d063abef61bf4ae05c3d84
mohit kumar <mohit@joshtalks.com>
2019-12-13 16:07:54 +0530

    new build release 1.0.22

---
+++ app/src/main/java/com/joshtalks/joshskills/core/chrome/BrowserActionsReceiver.java
@@ -0,0 +1,19 @@
+package com.joshtalks.joshskills.core.chrome;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * A {@link BroadcastReceiver} that handles the callback if default menu items are chosen from
+ * Browser Actions.
+ */
+public class BrowserActionsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String toastMsg = "Chosen item Id: " + intent.getDataString();
+       // Toast.makeText(context, toastMsg, Toast.LENGTH_SHORT).show();
+        Log.e("BrowserActionsReceiver",toastMsg);
+    }
+}
