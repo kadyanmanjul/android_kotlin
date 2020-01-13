abc2329b0394d11e5240b512d8ee8a606684725a
mohit kumar <mohit@joshtalks.com>
2020-01-13 14:01:21 +0530

    ADD database migration

--- app/src/main/java/com/joshtalks/joshskills/repository/local/AppDatabase.kt
+++ app/src/main/java/com/joshtalks/joshskills/repository/local/AppDatabase.kt
@@ -42,7 +42,8 @@ abstract class AppDatabase : RoomDatabase() {
                             context.applicationContext,
                             AppDatabase::class.java, DATABASE_NAME
                         )
-                            .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4,MIGRATION_4_5)
+                            .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4,MIGRATION_4_5,
+                                MIGRATION_5_6)
                             .fallbackToDestructiveMigration()
                             .addCallback(sRoomDatabaseCallback)
                             .build()
