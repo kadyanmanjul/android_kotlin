6c3945ed86dae6a4ede666b25652520797b18622
mohit kumar <mohit@joshtalks.com>
2020-02-22 12:52:48 +0530

    remove migration 7 to 8

--- app/src/main/java/com/joshtalks/joshskills/repository/local/AppDatabase.kt
+++ app/src/main/java/com/joshtalks/joshskills/repository/local/AppDatabase.kt
@@ -47,7 +47,8 @@ abstract class AppDatabase : RoomDatabase() {
                         )
                             .addMigrations(
                                 MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4, MIGRATION_4_5,
-                                MIGRATION_5_6, MIGRATION_6_7, MIGRATION_7_8,
+                                MIGRATION_5_6, MIGRATION_6_7,
+                                //MIGRATION_7_8,
                                 MIGRATION_8_9
                             )
                             .fallbackToDestructiveMigration()
@@ -101,12 +102,12 @@ abstract class AppDatabase : RoomDatabase() {
             }
         }
 
-        private val MIGRATION_7_8: Migration = object : Migration(7, 8) {
+       /* private val MIGRATION_7_8: Migration = object : Migration(7, 8) {
             override fun migrate(database: SupportSQLiteDatabase) {
                 database.execSQL("ALTER TABLE course ADD COLUMN chat_type TEXT ")
                 database.execSQL("ALTER TABLE question_table ADD COLUMN type TEXT ")
             }
-        }
+        }*/
         private val MIGRATION_8_9: Migration = object : Migration(8, 9) {
             override fun migrate(database: SupportSQLiteDatabase) {
                 try {
