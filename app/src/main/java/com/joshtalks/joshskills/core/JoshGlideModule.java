2b3a6078ed223c1f65607c2673cb6db8f68d0804
mohit kumar <mohit@joshtalks.com>
2020-02-28 14:18:42 +0530

    build 1.0.65 release

--- app/src/main/java/com/joshtalks/joshskills/core/JoshGlideModule.java
+++
@@ -1,8 +0,0 @@
-package com.joshtalks.joshskills.core;
-
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-@GlideModule
-public final class JoshGlideModule extends AppGlideModule {
-}
---
+++ app/src/main/java/com/joshtalks/joshskills/core/glidemodules/GlideModulesUtil.kt
@@ -0,0 +1,12 @@
+package com.joshtalks.joshskills.core.glidemodules
+/*
+
+import com.bumptech.glide.annotation.GlideExtension
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+
+@GlideModule
+class GlideModules : AppGlideModule()
+
+*/
