0520ea2a1471ec2d233f99153741f17034b345be
Mohit Kumar <mohit@joshtalks.com>
2020-05-04 15:15:19 +0530

    Add configure  orientation for all activity

--- app/src/main/java/com/joshtalks/joshskills/core/CoreJoshActivity.kt
+++ app/src/main/java/com/joshtalks/joshskills/core/CoreJoshActivity.kt
@@ -2,15 +2,18 @@ package com.joshtalks.joshskills.core
 
 import android.app.Activity
 import android.content.Intent
-import android.view.View
-import com.joshtalks.joshskills.R
+import android.content.pm.ActivityInfo
+import android.os.Build
+import android.os.Bundle
 import com.joshtalks.joshskills.core.analytics.AnalyticsEvent
 import com.joshtalks.joshskills.core.analytics.AppAnalytics
 import com.joshtalks.joshskills.repository.server.engage.Graph
 import com.joshtalks.joshskills.repository.service.EngagementNetworkHelper
 import com.joshtalks.joshskills.ui.explore.CourseExploreActivity
+import com.joshtalks.joshskills.ui.feedback.FeedbackFragment
 import com.joshtalks.joshskills.ui.inbox.COURSE_EXPLORER_WITHOUT_CODE
 import com.joshtalks.joshskills.ui.referral.PromotionDialogFragment
+import kotlinx.android.synthetic.main.base_toolbar.*
 
 abstract class CoreJoshActivity : BaseActivity() {
     protected var countUpTimer = CountUpTimer(true)
@@ -19,13 +22,19 @@ abstract class CoreJoshActivity : BaseActivity() {
     protected var currentAudio: String? = null
     protected var cAudioId: String? = null
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        requestedOrientation = if (Build.VERSION.SDK_INT == 26) {
+            ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+        } else {
+            ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onStart() {
         super.onStart()
         try {
-            findViewById<View>(R.id.iv_help).setOnClickListener {
-                openHelpActivity()
-            }
+            iv_help.setOnClickListener { openHelpActivity() }
         } catch (ex: Exception) {
         }
     }
@@ -74,20 +83,14 @@ abstract class CoreJoshActivity : BaseActivity() {
             .show(supportFragmentManager, "promotion_show_dialog")
         this.intent = null
     }
-
-}
-
-/*object RxBus22 {
-    private val publisher = ReplaySubject.create<Any>()
-
-    @JvmStatic
-    fun publish(event: Any) {
-        publisher.onNext(event)
+    fun openFeedbackFragment(){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        val prev = supportFragmentManager.findFragmentByTag("feedback_fragment_dialog")
+        if (prev != null) {
+            fragmentTransaction.remove(prev)
+        }
+        fragmentTransaction.addToBackStack(null)
+        FeedbackFragment.newInstance()
+            .show(supportFragmentManager, "feedback_fragment_dialog")
     }
-    val connectObserable = publisher.share().replay()
-
 }
-*/
-
-
-
