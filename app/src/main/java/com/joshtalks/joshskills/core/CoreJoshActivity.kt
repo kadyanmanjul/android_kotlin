2cad27f065615e3bbc2d05ab05dba62b481a6c41
Mohit Kumar <mohit@joshtalks.com>
2020-05-08 13:49:33 +0530

    Add minimum feedback in a day logic

--- app/src/main/java/com/joshtalks/joshskills/core/CoreJoshActivity.kt
+++ app/src/main/java/com/joshtalks/joshskills/core/CoreJoshActivity.kt
@@ -7,13 +7,22 @@ import android.os.Build
 import android.os.Bundle
 import com.joshtalks.joshskills.core.analytics.AnalyticsEvent
 import com.joshtalks.joshskills.core.analytics.AppAnalytics
+import com.joshtalks.joshskills.repository.local.entity.ChatModel
 import com.joshtalks.joshskills.repository.server.engage.Graph
+import com.joshtalks.joshskills.repository.server.feedback.FeedbackTypes
 import com.joshtalks.joshskills.repository.service.EngagementNetworkHelper
+import com.joshtalks.joshskills.ui.chat.PRACTISE_SUBMIT_REQUEST_CODE
+import com.joshtalks.joshskills.ui.chat.VIDEO_OPEN_REQUEST_CODE
 import com.joshtalks.joshskills.ui.explore.CourseExploreActivity
 import com.joshtalks.joshskills.ui.feedback.FeedbackFragment
 import com.joshtalks.joshskills.ui.inbox.COURSE_EXPLORER_WITHOUT_CODE
+import com.joshtalks.joshskills.ui.practise.PRACTISE_OBJECT
 import com.joshtalks.joshskills.ui.referral.PromotionDialogFragment
+import com.joshtalks.joshskills.ui.video_player.VIDEO_OBJECT
 import kotlinx.android.synthetic.main.base_toolbar.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 abstract class CoreJoshActivity : BaseActivity() {
     protected var countUpTimer = CountUpTimer(true)
@@ -83,14 +92,60 @@ abstract class CoreJoshActivity : BaseActivity() {
             .show(supportFragmentManager, "promotion_show_dialog")
         this.intent = null
     }
-    fun openFeedbackFragment(){
+
+    protected fun openFeedbackFragment(feedbackTypes: FeedbackTypes, questionId: String) {
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         val prev = supportFragmentManager.findFragmentByTag("feedback_fragment_dialog")
         if (prev != null) {
             fragmentTransaction.remove(prev)
         }
         fragmentTransaction.addToBackStack(null)
-        FeedbackFragment.newInstance()
+        FeedbackFragment.newInstance(feedbackTypes, questionId)
             .show(supportFragmentManager, "feedback_fragment_dialog")
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                if (requestCode == PRACTISE_SUBMIT_REQUEST_CODE || requestCode == VIDEO_OPEN_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+                    var obj: ChatModel? = null
+                    var feedbackType: FeedbackTypes = FeedbackTypes.VIDEO
+                    if (data!!.hasExtra(VIDEO_OBJECT)) {
+                        obj = data.getParcelableExtra(VIDEO_OBJECT) as ChatModel
+                        feedbackType = FeedbackTypes.VIDEO
+                    } else if (data.hasExtra(PRACTISE_OBJECT)) {
+                        obj = data.getParcelableExtra(PRACTISE_OBJECT) as ChatModel
+                        feedbackType = FeedbackTypes.PRACTISE
+                    }
+                    obj?.question?.questionId?.run {
+                        canTakeRequestFeedbackFromUser(feedbackType, this)
+                    }
+                }
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+            }
+        }
+    }
+
+    private suspend fun canTakeRequestFeedbackFromUser(
+        feedbackTypes: FeedbackTypes,
+        questionId: String
+    ) {
+        val minFeedbackCount =
+            AppObjectController.getFirebaseRemoteConfig()
+                .getDouble("MINIMUM_FEEDBACK_IN_A_DAY_COUNT").toInt()
+        val todaySubmitCount =
+            AppObjectController.appDatabase.feedbackEngageModelDao().getTotalCountOfRows()
+        if (todaySubmitCount >= minFeedbackCount) {
+            return
+        }
+        val flag =
+            AppObjectController.appDatabase.chatDao().getFeedbackStatusOfQuestion(questionId)
+        if (flag != null && flag) {
+            openFeedbackFragment(feedbackTypes, questionId)
+        }
+
     }
 }
