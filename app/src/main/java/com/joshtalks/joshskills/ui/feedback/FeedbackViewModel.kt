32a84a5e6a726aae0345961d6de2093d2c4cd770
Mohit Kumar <mohit@joshtalks.com>
2020-05-08 13:38:44 +0530

    Update feedback flag in question table after feedback submit

--- app/src/main/java/com/joshtalks/joshskills/ui/feedback/FeedbackViewModel.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/feedback/FeedbackViewModel.kt
@@ -2,13 +2,12 @@ package com.joshtalks.joshskills.ui.feedback
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.crashlytics.android.Crashlytics
 import com.joshtalks.joshskills.R
-import com.joshtalks.joshskills.core.AppObjectController
-import com.joshtalks.joshskills.core.EMPTY
-import com.joshtalks.joshskills.core.JoshApplication
-import com.joshtalks.joshskills.core.showToast
+import com.joshtalks.joshskills.core.*
+import com.joshtalks.joshskills.repository.local.entity.FeedbackEngageModel
 import com.joshtalks.joshskills.repository.local.model.Mentor
 import com.joshtalks.joshskills.repository.server.feedback.UserFeedbackRequest
 import kotlinx.coroutines.Dispatchers
@@ -20,6 +19,7 @@ import java.net.UnknownHostException
 
 class FeedbackViewModel(application: Application) : AndroidViewModel(application) {
     var context: JoshApplication = getApplication()
+    val apiCallStatusLiveData: MutableLiveData<ApiCallStatus> = MutableLiveData()
 
     fun submitFeedback(questionId: String, rating: Float, keyword: String, eText: String = EMPTY) {
         viewModelScope.launch(Dispatchers.IO) {
@@ -34,10 +34,13 @@ class FeedbackViewModel(application: Application) : AndroidViewModel(application
                 val resp =
                     AppObjectController.commonNetworkService.postUserFeedback(userFeedbackRequest)
                 if (resp.isSuccessful) {
-
-                } else {
-                    showToast(context.getString(R.string.generic_message_for_error))
+                    AppObjectController.appDatabase.chatDao()
+                        .userSubmitFeedbackStatusUpdate(questionId)
                 }
+                AppObjectController.appDatabase.feedbackEngageModelDao()
+                    .insertFeedbackEngage(FeedbackEngageModel(questionId))
+                apiCallStatusLiveData.postValue(ApiCallStatus.SUCCESS)
+                return@launch
             } catch (ex: Exception) {
                 when (ex) {
                     is HttpException -> {
@@ -50,6 +53,14 @@ class FeedbackViewModel(application: Application) : AndroidViewModel(application
                     }
                 }
             }
+            apiCallStatusLiveData.postValue(ApiCallStatus.FAILED)
+
+        }
+    }
+
+    fun updateQuestionFeedbackStatus(questionId: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            AppObjectController.appDatabase.chatDao().userSubmitFeedbackStatusUpdate(questionId)
         }
     }
 
