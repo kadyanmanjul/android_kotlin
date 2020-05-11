18e0f0da946c5c990539a501d79fcc70762688e1
Mohit Kumar <mohit@joshtalks.com>
2020-05-11 16:04:39 +0530

    update log exception

--- app/src/main/java/com/joshtalks/joshskills/core/analytics/LogException.kt
+++ app/src/main/java/com/joshtalks/joshskills/core/analytics/LogException.kt
@@ -4,6 +4,7 @@ import com.crashlytics.android.Crashlytics
 import com.joshtalks.joshskills.BuildConfig
 import com.newrelic.agent.android.NewRelic
 import io.sentry.core.Sentry
+import io.sentry.core.SentryLevel
 
 object LogException {
     fun catchException(throwable: Throwable) {
@@ -16,4 +17,21 @@ object LogException {
         Crashlytics.logException(throwable)
 
     }
+
+    fun catchError(tag: ErrorTag, error: String) {
+        if (BuildConfig.DEBUG) {
+            return
+        }
+        Sentry.captureMessage(tag.name + error, SentryLevel.ERROR)
+        NewRelic.recordCustomEvent(tag.name, mutableMapOf())
+        Crashlytics.log(3, tag.NAME, error)
+
+    }
 }
+
+enum class ErrorTag(val NAME: String) {
+    TRUE_CALLER("TrueCaller"),
+    OTP_REQUEST("OTP Request")
+
+}
+
