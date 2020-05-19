9585360603a4b5b22398853a2fe1228284bf3c9a
Mohit Kumar <mohit@joshtalks.com>
2020-05-19 11:17:59 +0530

    Add key LOGIN_ON who store login time of user

--- app/src/main/java/com/joshtalks/joshskills/core/PrefManager.kt
+++ app/src/main/java/com/joshtalks/joshskills/core/PrefManager.kt
@@ -16,6 +16,7 @@ const val SERVER_GID_ID = "server_gid_id"
 const val CERTIFICATE_GENERATE = "_certificate_generate"
 const val RATING_DETAILS_KEY = "_rating_details"
 const val CUSTOM_PERMISSION_ACTION_KEY = "joshskills_custom_permission"
+const val LOGIN_ON = "_login_on"
 
 object PrefManager {
 
