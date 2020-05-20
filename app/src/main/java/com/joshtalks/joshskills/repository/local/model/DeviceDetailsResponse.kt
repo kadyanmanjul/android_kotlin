d1ba814dbf3997f37e06b93eea301ed31d0b24dc
Mohit Kumar <mohit@joshtalks.com>
2020-05-20 13:49:51 +0530

    Add response class for device details api

---
+++ app/src/main/java/com/joshtalks/joshskills/repository/local/model/DeviceDetailsResponse.kt
@@ -0,0 +1,60 @@
+package com.joshtalks.joshskills.repository.local.model
+
+
+import com.google.gson.annotations.SerializedName
+import com.joshtalks.joshskills.core.AppObjectController
+import com.joshtalks.joshskills.core.PrefManager
+
+const val DEVICE_DETAILS_KEY = "device_details"
+
+data class DeviceDetailsResponse(
+    @SerializedName("app_version_code")
+    val appVersionCode: Int,
+    @SerializedName("app_version_name")
+    val appVersionName: String,
+    @SerializedName("brand")
+    val brand: String,
+    @SerializedName("device")
+    val device: String,
+    @SerializedName("device_id")
+    val deviceId: String,
+    @SerializedName("gaid_id")
+    val gaidId: String,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("manufacture")
+    val manufacture: String,
+    @SerializedName("mentor_id")
+    val mentorId: String,
+    @SerializedName("model")
+    val model: String,
+    @SerializedName("os_version_code")
+    val osVersionCode: Int,
+    @SerializedName("os_version_name")
+    val osVersionName: String
+) {
+    companion object {
+
+        @JvmStatic
+        fun getInstance(): DeviceDetailsResponse? {
+            return try {
+                AppObjectController.gsonMapper.fromJson(
+                    PrefManager.getStringValue(DEVICE_DETAILS_KEY),
+                    DeviceDetailsResponse::class.java
+                )
+            } catch (ex: Exception) {
+                null
+            }
+        }
+    }
+
+    fun update() {
+        val string: String = toString()
+        PrefManager.put(DEVICE_DETAILS_KEY, string)
+    }
+
+
+    override fun toString(): String {
+        return AppObjectController.gsonMapper.toJson(this)
+    }
+}
