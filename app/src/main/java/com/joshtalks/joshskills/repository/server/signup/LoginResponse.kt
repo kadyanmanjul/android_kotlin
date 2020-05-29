cba1fce02231d06a1c8e0d5b5f999edf553d08d3
Mohit Kumar <mohit@joshtalks.com>
2020-05-29 15:03:34 +0530

    LoginResponse.kt -> Refactor login response class

--- app/src/main/java/com/joshtalks/joshskills/repository/server/signup/LoginResponse.kt
+++ app/src/main/java/com/joshtalks/joshskills/repository/server/signup/LoginResponse.kt
@@ -1,24 +1,24 @@
-package com.joshtalks.joshskills.repository.server
+package com.joshtalks.joshskills.repository.server.signup
 
 
 import com.google.gson.annotations.SerializedName
 import com.joshtalks.joshskills.core.EMPTY
 
 data class LoginResponse(
-    @SerializedName("first_name")
-    val firstName: String,
     @SerializedName("id")
     val userId: String,
     @SerializedName("mentor_id")
     val mentorId: String,
+    @SerializedName("instance_id")
+    val instance_id: String,
     @SerializedName("mobile")
     val mobile: String,
     @SerializedName("token")
     val token: String,
     @SerializedName("referral_code")
     val referralCode: String = EMPTY,
-    @SerializedName("is_user_exist")
-    val isUserExist: Boolean
-
-
+    @SerializedName("new_user")
+    val newUser: Boolean,
+    @SerializedName("created_source")
+    val createdSource: String
 )
