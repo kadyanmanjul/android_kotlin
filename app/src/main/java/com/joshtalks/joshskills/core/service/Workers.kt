72e7c2f27bf3fe0efc23b04e0cfbc4c150d8ba41
Mohit Kumar <mohit@joshtalks.com>
2020-06-04 17:27:48 +0530

    Add worker for sync nps feedback

--- app/src/main/java/com/joshtalks/joshskills/core/service/Workers.kt
+++ app/src/main/java/com/joshtalks/joshskills/core/service/Workers.kt
@@ -22,6 +22,7 @@ import com.joshtalks.joshskills.repository.local.entity.NPSEventModel
 import com.joshtalks.joshskills.repository.local.eventbus.NPSEventGenerateEventBus
 import com.joshtalks.joshskills.repository.local.model.*
 import com.joshtalks.joshskills.repository.server.MessageStatusRequest
+import com.joshtalks.joshskills.repository.server.NPSByUserRequest
 import com.joshtalks.joshskills.repository.server.UpdateDeviceRequest
 import com.joshtalks.joshskills.repository.service.NetworkRequestHelper
 import com.joshtalks.joshskills.repository.service.SyncChatService
@@ -710,6 +711,19 @@ class MergeMentorWithGAIDWorker(context: Context, workerParams: WorkerParameters
     }
 }
 
+class SyncSavedNPSFeedbackWorker(context: Context, workerParams: WorkerParameters) :
+    CoroutineWorker(context, workerParams) {
+    override suspend fun doWork(): Result {
+        try {
+            NPSByUserRequest.getInstance()?.run {
+                AppObjectController.commonNetworkService.submitNPSResponse(this)
+                NPSByUserRequest.update(EMPTY)
+            }
+        } catch (ex: Throwable) {
+        }
+        return Result.success()
+    }
+}
 
 fun getGoogleAdId(context: Context): String {
     MobileAds.initialize(context)
