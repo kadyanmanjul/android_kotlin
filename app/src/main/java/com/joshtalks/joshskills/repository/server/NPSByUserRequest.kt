31e9bbbcef53e8aec6306401c0b62f464eb99eaa
Mohit Kumar <mohit@joshtalks.com>
2020-06-05 15:34:13 +0530

    NPS request  model change

--- app/src/main/java/com/joshtalks/joshskills/repository/server/NPSByUserRequest.kt
+++ app/src/main/java/com/joshtalks/joshskills/repository/server/NPSByUserRequest.kt
@@ -1,13 +1,10 @@
 package com.joshtalks.joshskills.repository.server
 
-
 import com.google.gson.annotations.SerializedName
-import com.joshtalks.joshskills.core.AppObjectController
-import com.joshtalks.joshskills.core.PrefManager
-
-const val NPS_REQUEST_WITHOUT_LOGIN = "nps_request_without_login"
-
 data class NPSByUserRequest(
+
+    @SerializedName("instance")
+    val instanceId: String?,
     @SerializedName("mentor")
     val mentor: String,
     @SerializedName("event_name")
@@ -15,25 +12,7 @@ data class NPSByUserRequest(
     @SerializedName("rating")
     val rating: Int,
     @SerializedName("text")
-    val text: String?
-) {
-    companion object {
-
-        @JvmStatic
-        fun getInstance(): NPSByUserRequest? {
-            return try {
-                AppObjectController.gsonMapper.fromJson(
-                    PrefManager.getStringValue(NPS_REQUEST_WITHOUT_LOGIN),
-                    NPSByUserRequest::class.java
-                )
-            } catch (ex: Exception) {
-                return null
-            }
-        }
-
-        fun update(value: String) {
-            PrefManager.put(NPS_REQUEST_WITHOUT_LOGIN, value)
-        }
-    }
-
-}
+    val text: String?,
+    @SerializedName("course")
+    val courseId: String?
+)
