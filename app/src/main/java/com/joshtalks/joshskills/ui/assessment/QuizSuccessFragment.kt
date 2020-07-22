e1fdb523d1824710bbf841c919717aeab0600060
Manjul Kadyan <manjulkadyan@joshtalks.com>
2020-07-22 13:07:52 +0530

    QuizSuccessFragment.kt, Fix button issue.

--- app/src/main/java/com/joshtalks/joshskills/ui/assessment/QuizSuccessFragment.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/assessment/QuizSuccessFragment.kt
@@ -4,20 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.joshtalks.joshskills.R
+import com.joshtalks.joshskills.databinding.FragmentQuizSuccessBinding
 
 class QuizSuccessFragment : Fragment() {
+    private lateinit var binding: FragmentQuizSuccessBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
+
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_quiz_success, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_quiz_success, container, false)
+        binding.lifecycleOwner = this
+        binding.fragment = this
+        return binding.root
     }
 
     fun backToCourse() {
-        //TODO
+        requireActivity().finish()
     }
 
     companion object {
