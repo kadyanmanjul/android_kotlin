0cd6c7c657a4a2d6b647f0c88dae49eab8a6ee47
Sahil Chopra <sahilchopra0003@gmail.com>
2020-07-31 14:35:09 +0530

    Merge Fixes

--- app/src/main/java/com/joshtalks/joshskills/core/Utils.kt
+++ app/src/main/java/com/joshtalks/joshskills/core/Utils.kt
@@ -775,17 +775,18 @@ fun showToast(message: String, length: Int = Toast.LENGTH_SHORT) {
 
 }
 
-fun getUserNameInShort(name: String = User.getInstance().firstName.trim().toUpperCase()): String {
+fun getUserNameInShort(
+    name: String = User.getInstance().firstName.trim().toUpperCase(Locale.ROOT)
+): String {
     return try {
         if (name.contains(" ")) {
             val nameSplit = name.split(" ")
             nameSplit[0][0].plus(nameSplit[1][0].toString())
         }
+        name.substring(0, 2)
+    } catch (e: IndexOutOfBoundsException) {
+        name.substring(0, name.length)
     }
-    name.substring(0, 2)
-} catch (e: IndexOutOfBoundsException) {
-    name.substring(0, name.length)
-}
 }
 
 fun hideKeyboard(activity: Activity, view: View) {
