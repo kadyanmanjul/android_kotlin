4fe977cbea316d80765255688a86ff5482b65b3e
Sahil Chopra <sahilchopra0003@gmail.com>
2020-08-01 16:04:03 +0530

    CourseExploreModel.kt, Add new params to ExploreCourseModel

--- app/src/main/java/com/joshtalks/joshskills/repository/server/CourseExploreModel.kt
+++ app/src/main/java/com/joshtalks/joshskills/repository/server/CourseExploreModel.kt
@@ -4,41 +4,62 @@ package com.joshtalks.joshskills.repository.server
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import com.joshtalks.joshskills.core.EMPTY
+import com.joshtalks.joshskills.repository.local.model.ExploreCardType
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class CourseExploreModel(
+
     @SerializedName("amount")
     var amount: Double,
+
     @SerializedName("batch_type")
     val batchType: Int,
+
     @SerializedName("course")
     var course: Int,
+
     @SerializedName("created")
     val created: String,
+
     @SerializedName("id")
     var id: Int? = -1,
+
     @SerializedName("is_default")
     val isDefault: Boolean,
+
     @SerializedName("modified")
     val modified: String,
+
     @SerializedName("test_name")
     val testName: String,
+
     @SerializedName("thumbnail")
     var imageUrl: String,
+
     @SerializedName("course_name")
     var courseName: String,
+
     @SerializedName("course_duration")
     val courseDuration: String,
+
     @SerializedName("course_icon")
     var courseIcon: String,
+
     @SerializedName("whatsapp_url")
     var whatsappUrl: String?,
+
     @SerializedName("language")
     var language: String?,
-    @SerializedName("certificate")
-    val certificate: Boolean = false
 
+    @SerializedName("certificate")
+    val certificate: Boolean = false,
+
+    @SerializedName("is_clickable")
+    val isClickable: Boolean = true,
+
+    @SerializedName("card_type")
+    val cardType: ExploreCardType = ExploreCardType.NORMAL
 
 ) : Parcelable {
 
@@ -57,7 +78,9 @@ data class CourseExploreModel(
         courseIcon = EMPTY,
         whatsappUrl = EMPTY,
         language = EMPTY,
-        certificate = false
+        certificate = false,
+        isClickable = true,
+        cardType = ExploreCardType.NORMAL
     )
 
 }
