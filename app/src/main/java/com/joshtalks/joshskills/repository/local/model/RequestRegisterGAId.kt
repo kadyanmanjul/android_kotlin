7de15b83c36d076572073764e41f55e9282ff7cb
Sahil Chopra <sahilchopra0003@gmail.com>
2020-08-01 12:40:03 +0530

    RequestRegisterGAId.kt, Rearranged Code

--- app/src/main/java/com/joshtalks/joshskills/repository/local/model/RequestRegisterGAId.kt
+++ app/src/main/java/com/joshtalks/joshskills/repository/local/model/RequestRegisterGAId.kt
@@ -6,22 +6,31 @@ import com.google.gson.annotations.SerializedName
 import com.joshtalks.joshskills.core.EMPTY
 
 data class RequestRegisterGAId(
+
     @SerializedName("gaid")
     var gaid: String = EMPTY,
+
     @SerializedName("install_on")
     var installOn: Long = 0,
+
     @SerializedName("other_info")
     val otherInfo: Map<String, String> = emptyMap(),
+
     @SerializedName("test")
     var test: Int = 0,
+
     @SerializedName("utm_medium")
     var utmMedium: String = EMPTY,
+
     @SerializedName("utm_source")
     var utmSource: String = EMPTY,
+
     @Expose(serialize = false, deserialize = true)
     var id: Int = 0,
+
     @SerializedName("explore_type")
     var exploreType: ExploreType = ExploreType.NORMAL
+
 )
 
 enum class ExploreType(value: String) {
