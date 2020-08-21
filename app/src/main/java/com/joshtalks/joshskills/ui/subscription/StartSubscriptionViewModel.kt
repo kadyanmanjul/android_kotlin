2c9fb14c1d5fa54fc71b602e736aef2ea0375e8f
Sahil Chopra <sahilchopra0003@gmail.com>
2020-08-21 14:21:33 +0530

    StartSubscriptionViewModel.kt, Register test-impression

--- app/src/main/java/com/joshtalks/joshskills/ui/subscription/StartSubscriptionViewModel.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/subscription/StartSubscriptionViewModel.kt
@@ -6,19 +6,42 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.joshtalks.joshskills.core.ApiCallStatus
 import com.joshtalks.joshskills.core.AppObjectController
+import com.joshtalks.joshskills.core.INSTANCE_ID
 import com.joshtalks.joshskills.core.PrefManager
 import com.joshtalks.joshskills.core.USER_UNIQUE_ID
+import com.joshtalks.joshskills.repository.local.model.Mentor
 import com.joshtalks.joshskills.repository.server.CourseExploreModel
 import com.joshtalks.joshskills.util.showAppropriateMsg
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import java.util.HashMap
 
 class StartSubscriptionViewModel(application: Application) : AndroidViewModel(application) {
 
     private val jobs = arrayListOf<Job>()
     val apiCallStatusLiveData: MutableLiveData<ApiCallStatus> = MutableLiveData()
     val subscriptionTestDetailsLiveData: MutableLiveData<CourseExploreModel> = MutableLiveData()
+
+    private suspend fun registerTestImpression(testId: String) {
+        try {
+            val requestParams: HashMap<String, String> = HashMap()
+            requestParams["test_id"] = testId
+            requestParams["gaid"] = PrefManager.getStringValue(USER_UNIQUE_ID)
+            requestParams["instance_id"] = PrefManager.getStringValue(INSTANCE_ID, true)
+            if (Mentor.getInstance().getId().isNotEmpty()) {
+                requestParams["mentor_id"] = Mentor.getInstance().getId()
+            }
+            val response =
+                AppObjectController.commonNetworkService.getCourseDetails(requestParams)
+            if (response.isSuccessful) {
+                return
+            }
+
+        } catch (ignored: Throwable) {
+            // Do Nothing
+        }
+    }
 
     fun fetchSubscriptionDetails() {
         jobs += viewModelScope.launch(Dispatchers.IO) {
@@ -27,6 +50,7 @@ class StartSubscriptionViewModel(application: Application) : AndroidViewModel(ap
                 val resp =
                     AppObjectController.signUpNetworkService.getSubscriptionTestDetails(gaid)
                 if (resp.isSuccessful && resp.body() != null) {
+                    registerTestImpression(resp.body()!!.id.toString())
                     apiCallStatusLiveData.postValue(ApiCallStatus.SUCCESS)
                     subscriptionTestDetailsLiveData.postValue(resp.body())
                     return@launch
