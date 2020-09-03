d595e50225134f2a5e49be83c6695f38320f164a
Deepak Kumar <deepakkumar@joshtalks.com>
2020-09-03 17:05:10 +0530

    OnboardingResponse.kt, api response model creation and interest fragment design.

---
+++ app/src/main/java/com/joshtalks/joshskills/repository/server/onboarding/OnboardingResponse.kt
@@ -0,0 +1,74 @@
+package com.joshtalks.joshskills.repository.server.onboarding
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class OnboardingResponse(
+    @SerializedName("version")
+    @Expose
+    var version: Version? = null,
+
+    @SerializedName("image")
+    @Expose
+    var image: String? = null,
+
+    @SerializedName("content")
+    @Expose
+    var content: List<Content>? = null,
+
+    @SerializedName("maximum_number_of_interests")
+    @Expose
+    var maximumNumberOfInterests: Int? = null,
+
+    @SerializedName("minimum_number_of_interests")
+    @Expose
+    var minimumNumberOfInterests: Int? = null,
+
+    @SerializedName("interest_text")
+    @Expose
+    var interestText: String? = null,
+
+    @SerializedName("course_interest_tags")
+    @Expose
+    var courseInterestTags: List<CourseInterestTag>? = null,
+
+    @SerializedName("course_categories")
+    @Expose
+    var courseCategories: List<CourseCategory>? = null
+)
+
+data class CourseInterestTag(
+    @SerializedName("id")
+    @Expose
+    var id: Int? = null,
+    @SerializedName("name")
+    @Expose
+    var name: String? = null
+)
+
+data class Content(
+    @SerializedName("text")
+    @Expose
+    var text: String? = null,
+    @SerializedName("description")
+    @Expose
+    var description: String? = null
+)
+
+data class CourseCategory(
+    @SerializedName("id")
+    @Expose
+    var id: Int? = null,
+    @SerializedName("name")
+    @Expose
+    var name: String? = null
+)
+
+class Version(
+    @SerializedName("id")
+    @Expose
+    var id: Int? = null,
+    @SerializedName("name")
+    @Expose
+    var name: String? = null
+)
--- app/src/main/java/com/joshtalks/joshskills/ui/new_onboarding/onboarding2/SelectInterestFragment.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/new_onboarding/onboarding2/SelectInterestFragment.kt
@@ -1,21 +1,54 @@
 package com.joshtalks.joshskills.ui.new_onboarding.onboarding2
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import com.joshtalks.joshskills.R
+import com.joshtalks.joshskills.databinding.FragmentSelectInterestBinding
 
 class SelectInterestFragment : Fragment() {
+
+    lateinit var binding: FragmentSelectInterestBinding
 
     companion object {
         fun newInstance(
             maxInterest: Int,
-            minInterest: Int,
-            interestList: List<String>
+            minInterest: Int
         ): SelectInterestFragment {
             val args = Bundle()
 
             val fragment = SelectInterestFragment()
             fragment.arguments = args
             return fragment
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_select_interest, container, false)
+
+        binding.interestCg.setOnCheckedChangeListener(ChipGroup.OnCheckedChangeListener { group, checkedId -> })
+        populateInterests()
+        return binding.root
+    }
+
+    private fun populateInterests() {
+        for (i in 1..10) {
+            val chip = LayoutInflater.from(context)
+                .inflate(R.layout.faq_category_item, null, false) as Chip
+            chip.text = "Category Name"
+            chip.tag = i
+            chip.id = i
+            binding.interestCg.addView(chip)
         }
     }
 }
