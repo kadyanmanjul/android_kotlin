93f989819d180f44d6c4f79686660bbe0eac33dc
Deepak Kumar <deepakkumar@joshtalks.com>
2020-09-11 12:58:01 +0530

    ReminderActivity.kt, Reminder Events added.

--- app/src/main/java/com/joshtalks/joshskills/ui/inbox/InboxViewModel.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/inbox/InboxViewModel.kt
@@ -132,7 +132,7 @@ class InboxViewModel(application: Application) : AndroidViewModel(application) {
                                         hours.toIntOrNull(),
                                         mins.toIntOrNull()
                                     )
-                                    }
+                                }
 
                                 }
                             } else
