422c8a5f547916f9504b82a29255161b953ee1e6
Deepak Kumar <deepakkumar@joshtalks.com>
2020-10-09 18:48:29 +0530

    image_dialog_fragment.xml, data set.

--- dialogflowchatlibrary/src/main/java/com/tyagiabhinav/dialogflowchatlibrary/ImageDialogFragment.kt
+++ dialogflowchatlibrary/src/main/java/com/tyagiabhinav/dialogflowchatlibrary/ImageDialogFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.tyagiabhinav.dialogflowchatlibrary.databinding.ImageDialogFragmentBinding
 
 const val TRIAL_TEST_ID = 13
@@ -13,6 +15,20 @@ const val TRIAL_TEST_ID = 13
 class ImageDialogFragment : DialogFragment() {
 
     private lateinit var binding: ImageDialogFragmentBinding
+
+    var title: String? = null
+    var description: String? = null
+    var imageUrl: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            title = it.getString("title")
+            description = it.getString("description")
+            imageUrl = it.getString("imgUrl")
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,7 +42,19 @@ class ImageDialogFragment : DialogFragment() {
         )
         binding.lifecycleOwner = this
         binding.handler = this
+
+        updateUi()
         return binding.root
+    }
+
+    private fun updateUi() {
+        binding.titleTv.text = title
+        binding.timeTv.visibility = View.GONE
+        binding.description.text = description
+        Glide.with(requireContext())
+            .load(imageUrl)
+            .apply(RequestOptions.circleCropTransform())
+            .into(binding.courseIv)
     }
 
     fun cancel() {
