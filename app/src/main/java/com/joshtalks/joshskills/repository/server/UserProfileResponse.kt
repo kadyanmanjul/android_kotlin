a5bb6ab111d5f9ec0b8c586fb8a4d35ed24c37a8
Manjul Kadyan <manjulkadyan@joshtalks.com>
2020-11-27 12:52:50 +0530

    UserProfileResponse.kt,add new parameter

--- app/src/main/java/com/joshtalks/joshskills/repository/server/UserProfileResponse.kt
+++ app/src/main/java/com/joshtalks/joshskills/repository/server/UserProfileResponse.kt
@@ -67,7 +67,9 @@ data class Certificate(
     @SerializedName("sort_order")
     val sortOrder: Int?,
     @SerializedName("certificate_description")
-    val certificateDescription: String?
+    val certificateDescription: String?,
+    @SerializedName("is_achieved")
+    val is_achieved: Boolean=false
 )
 
 data class AwardCategory(
@@ -92,5 +94,7 @@ data class Award(
     @SerializedName("image_url")
     val imageUrl: String?,
     @SerializedName("award_description")
-    val awardDescription: String?
+    val awardDescription: String?,
+    @SerializedName("is_achieved")
+    val is_achieved: Boolean=false
 ) : Parcelable
