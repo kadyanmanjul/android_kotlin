b89a98c233589f8904c95fc8d7be0be30768ef2a
Manjul Kadyan <manjulkadyan@joshtalks.com>
2020-12-05 18:09:59 +0530

    Add dynamic new custom snackbar for points

--- app/src/main/java/com/joshtalks/joshskills/core/Utils.kt
+++ app/src/main/java/com/joshtalks/joshskills/core/Utils.kt
@@ -38,15 +38,18 @@ import android.util.Log
 import android.util.TypedValue
 import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.view.inputmethod.InputMethodManager
+import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.RoundedBitmapDrawable
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
@@ -1022,6 +1025,34 @@ fun String.getExtension(): String {
     return this.substring(this.lastIndexOf("."))
 }
 
+internal fun View?.findSuitableParent(): ViewGroup? {
+    var view = this
+    var fallback: ViewGroup? = null
+    do {
+        if (view is CoordinatorLayout) {
+            // We've found a CoordinatorLayout, use it
+            return view
+        } else if (view is FrameLayout) {
+            if (view.id == android.R.id.content) {
+                // If we've hit the decor content view, then we didn't find a CoL in the
+                // hierarchy, so use it.
+                return view
+            } else {
+                // It's not the content view but we'll use it as our fallback
+                fallback = view
+            }
+        }
+
+        if (view != null) {
+            // Else, we will loop and crawl up the view hierarchy and try to find a parent
+            val parent = view.parent
+            view = if (parent is View) parent else null
+        }
+    } while (view != null)
+
+    // If we reach here then we didn't find a CoL or a suitable content view so we'll fallback
+    return fallback
+}
 
 fun Intent.serviceStart() {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
---
+++ app/src/main/java/com/joshtalks/joshskills/core/custom_ui/PointSnackbar.kt
@@ -0,0 +1,59 @@
+package com.joshtalks.joshskills.core.custom_ui
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.joshtalks.joshskills.R
+import com.joshtalks.joshskills.core.findSuitableParent
+
+class PointSnackbar(
+    parent: ViewGroup,
+    content: PointsSnackbarView
+) : BaseTransientBottomBar<PointSnackbar>(parent, content, content) {
+
+    init {
+        getView().setBackgroundColor(
+            ContextCompat.getColor(
+                view.context,
+                android.R.color.transparent
+            )
+        )
+        getView().setPadding(0, 0, 0, 0)
+    }
+
+    companion object {
+
+        fun make(view: View, duration: Int, action_lable: String?): PointSnackbar? {
+
+            // First we find a suitable parent for our custom view
+            val parent = view.findSuitableParent() ?: throw IllegalArgumentException(
+                "No suitable parent found from the given view. Please provide a valid view."
+            )
+
+            // We inflate our custom view
+            try {
+                val customView = LayoutInflater.from(view.context).inflate(
+                    R.layout.point_snackbar_view,
+                    parent,
+                    false
+                ) as PointsSnackbarView
+                // We create and return our Snackbar
+                action_lable?.let {
+                    customView.tvMsg.text = action_lable
+                }
+
+                return PointSnackbar(
+                    parent,
+                    customView
+                ).setDuration(duration)
+            } catch (e: Exception) {
+                Log.v("exception ", e.message)
+            }
+
+            return null
+        }
+    }
+}
