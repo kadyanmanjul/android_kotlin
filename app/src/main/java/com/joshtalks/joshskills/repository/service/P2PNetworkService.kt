814935ef29cbc8d4e05ffaa7e121b804bfc8775a
Mohit Kumar <mohitkumar@joshtalks.com>
2020-12-21 18:13:44 +0530

    add api for start and stop recording of agora

--- app/src/main/java/com/joshtalks/joshskills/repository/service/P2PNetworkService.kt
+++ app/src/main/java/com/joshtalks/joshskills/repository/service/P2PNetworkService.kt
@@ -1,7 +1,9 @@
 package com.joshtalks.joshskills.repository.service
 
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import java.util.HashMap
 
 interface P2PNetworkService {
@@ -11,5 +13,15 @@ interface P2PNetworkService {
 
     @POST("$DIR/voicecall/agora_call_response/")
     suspend fun getAgoraCallResponse(@Body params: Map<String, String?>)
+
+    @POST("$DIR/voicecall/agora_start_recording/")
+    suspend fun startP2PCallRecording(@Body params: Map<String, String?>): HashMap<String, String>
+
+    @POST("$DIR/voicecall/agora_stop_recording/")
+    suspend fun stopP2PCallRecording(@Body params: Map<String, String?>): Any
+
+    @GET("$DIR/voicecall/agora_user_profile/{id}/")
+    suspend fun getUserDetailOnCall(@Path("id") id: String): HashMap<String, String>
+
 
 }
