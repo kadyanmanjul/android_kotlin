a155d7ed4b983e005cd293553e830f509b81f399
Manjul Kadyan <manjulkadyan@joshtalks.com>
2020-12-25 14:00:35 +0530

    Remove points features

--- app/src/main/java/com/joshtalks/joshskills/ui/inbox/InboxActivity.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/inbox/InboxActivity.kt
@@ -726,8 +726,8 @@ class InboxActivity : CoreJoshActivity(), LifecycleObserver, InAppUpdateManager.
 
     private fun initScoreCardView(userData: UserProfileResponse) {
         userData.isPointsActive?.let { isLeaderBoardActive ->
-            PrefManager.put(IS_LEADERBOARD_ACTIVE, userData.isPointsActive)
-                if (userData.isPointsActive) {
+            PrefManager.put(IS_LEADERBOARD_ACTIVE, false)
+                if (false) {
                     user_data_container.visibility = View.VISIBLE
                     user_points.text = userData.points.toString()
                     user_streak_data.text = userData.streak.toString()
