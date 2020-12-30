bd35d74579892199884c2dc14bb90c178109d89c
Mohit Kumar <mohitkumar@joshtalks.com>
2020-12-30 13:37:32 +0530

    Build 3.1.9 Release

--- app/src/main/java/com/joshtalks/joshskills/core/videoplayer/BitVideoPlayer.kt
+++ app/src/main/java/com/joshtalks/joshskills/core/videoplayer/BitVideoPlayer.kt
@@ -15,14 +15,7 @@ import android.os.Build
 import android.os.Handler
 import android.util.AttributeSet
 import android.util.Pair
-import android.view.LayoutInflater
-import android.view.MotionEvent
-import android.view.Surface
-import android.view.TextureView
-import android.view.View
-import android.view.ViewGroup
-import android.view.Window
-import android.view.WindowManager
+import android.view.*
 import android.view.animation.DecelerateInterpolator
 import android.widget.ProgressBar
 import androidx.appcompat.widget.AppCompatImageView
@@ -40,31 +33,15 @@ import com.afollestad.materialdialogs.LayoutMode
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.bottomsheets.BottomSheet
 import com.afollestad.materialdialogs.customview.customView
-import com.google.android.exoplayer2.C
-import com.google.android.exoplayer2.DefaultLoadControl
+import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.DefaultLoadControl.DEFAULT_VIDEO_BUFFER_SIZE
-import com.google.android.exoplayer2.DefaultRenderersFactory
-import com.google.android.exoplayer2.ExoPlaybackException
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.PlaybackParameters
-import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.Player.STATE_BUFFERING
 import com.google.android.exoplayer2.Player.STATE_IDLE
-import com.google.android.exoplayer2.RenderersFactory
-import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.audio.AudioAttributes
 import com.google.android.exoplayer2.source.TrackGroupArray
-import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.trackselection.*
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector.ParametersBuilder
-import com.google.android.exoplayer2.trackselection.MappingTrackSelector
-import com.google.android.exoplayer2.trackselection.TrackSelection
-import com.google.android.exoplayer2.trackselection.TrackSelectionArray
-import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
-import com.google.android.exoplayer2.ui.DefaultTimeBar
-import com.google.android.exoplayer2.ui.PlayerControlView
-import com.google.android.exoplayer2.ui.PlayerView
-import com.google.android.exoplayer2.ui.TimeBar
+import com.google.android.exoplayer2.ui.*
 import com.google.android.exoplayer2.ui.TimeBar.OnScrubListener
 import com.google.android.exoplayer2.upstream.DefaultAllocator
 import com.google.android.exoplayer2.util.ErrorMessageProvider
@@ -76,8 +53,7 @@ import com.joshtalks.joshskills.core.analytics.AnalyticsEvent
 import com.joshtalks.joshskills.core.analytics.AppAnalytics
 import com.joshtalks.joshskills.core.service.video_download.VideoDownloadController
 import com.mikhaellopez.circularprogressbar.CircularProgressBar
-import java.util.Formatter
-import java.util.Locale
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 class BitVideoPlayer : PlayerView, LifecycleObserver, PlayerControlView.VisibilityListener,
@@ -296,19 +272,23 @@ class BitVideoPlayer : PlayerView, LifecycleObserver, PlayerControlView.Visibili
         }
 
         override fun onBeforeMove(dir: Direction) {
-            if (mGestureType != GestureType.SwipeGesture)
-                return
-            if (dir == Direction.LEFT || dir == Direction.RIGHT) {
-                pausePlayer()
-                mPositionTextView.visibility = View.VISIBLE
-            } else {
-                maxBrightness = 100
-                mWindow?.attributes?.let {
-                    startBrightness = (it.screenBrightness * 100).toInt()
+            try {
+                if (mGestureType != GestureType.SwipeGesture)
+                    return
+                if (dir == Direction.LEFT || dir == Direction.RIGHT) {
+                    pausePlayer()
+                    mPositionTextView.visibility = View.VISIBLE
+                } else {
+                    maxBrightness = 100
+                    mWindow?.attributes?.let {
+                        startBrightness = (it.screenBrightness * 100).toInt()
+                    }
+                    maxVolume = am?.getStreamMaxVolume(AudioManager.STREAM_MUSIC) ?: 100
+                    startVolume = am?.getStreamVolume(AudioManager.STREAM_MUSIC) ?: 100
+                    mPositionTextView.visibility = View.VISIBLE
                 }
-                maxVolume = am?.getStreamMaxVolume(AudioManager.STREAM_MUSIC) ?: 100
-                startVolume = am?.getStreamVolume(AudioManager.STREAM_MUSIC) ?: 100
-                mPositionTextView.visibility = View.VISIBLE
+            } catch (ex: java.lang.Exception) {
+                ex.printStackTrace()
             }
         }
 
