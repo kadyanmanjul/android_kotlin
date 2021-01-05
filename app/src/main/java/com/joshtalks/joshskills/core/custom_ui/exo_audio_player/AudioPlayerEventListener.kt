51a2bccc7377864d330655cd1070422114ff9931
Mohit Kumar <mohitkumar@joshtalks.com>
2021-01-05 16:18:20 +0530

    interface update

--- app/src/main/java/com/joshtalks/joshskills/core/custom_ui/exo_audio_player/AudioPlayerEventListener.kt
+++ app/src/main/java/com/joshtalks/joshskills/core/custom_ui/exo_audio_player/AudioPlayerEventListener.kt
@@ -1,13 +1,13 @@
 package com.joshtalks.joshskills.core.custom_ui.exo_audio_player
 
 interface AudioPlayerEventListener {
-    fun onPlayerPause()
-    fun onPlayerResume()
-    fun onCurrentTimeUpdated(lastPosition: Long)
-    fun onTrackChange(tag: String?)
-    fun onPositionDiscontinuity(lastPos: Long, reason: Int = 1)
-    fun onPlayerReleased()
-    fun onPlayerEmptyTrack()
-    fun onPositionDiscontinuity(reason: Int)
+    fun onPlayerPause() {}
+    fun onPlayerResume() {}
+    fun onCurrentTimeUpdated(lastPosition: Long) {}
+    fun onTrackChange(tag: String?) {}
+    fun onPositionDiscontinuity(lastPos: Long, reason: Int = 1) {}
+    fun onPlayerReleased() {}
+    fun onPlayerEmptyTrack() {}
+    fun onPositionDiscontinuity(reason: Int) {}
     fun complete()
 }
