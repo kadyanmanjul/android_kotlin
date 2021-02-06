bf98a91042726d4ee36c1b74a66f58616339536d
Manjul Kadyan <manjulkadyan@joshtalks.com>
2021-02-06 14:38:13 +0530

    Refactor Code

--- app/src/main/java/com/joshtalks/joshskills/core/CoreJoshFragment.kt
+++ app/src/main/java/com/joshtalks/joshskills/core/CoreJoshFragment.kt
@@ -1,11 +1,8 @@
 package com.joshtalks.joshskills.core
 
 import android.content.Context
-import android.media.MediaPlayer
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
-import com.joshtalks.joshskills.R
 import com.joshtalks.joshskills.core.custom_ui.PointSnackbar
 
 open class CoreJoshFragment : Fragment() {
@@ -24,18 +21,8 @@ open class CoreJoshFragment : Fragment() {
         if (PrefManager.getBoolValue(IS_PROFILE_FEATURE_ACTIVE)) {
             //SoundPoolManager.getInstance(AppObjectController.joshApplication).playSnackBarSound()
             PointSnackbar.make(view, duration, action_lable)?.show()
-            val mediaplayer: MediaPlayer = MediaPlayer.create(
-                requireActivity(),
-                R.raw.ting
-            ) //You Can Put Your File Name Instead Of abc
+            playSnackbarSound(requireActivity())
 
-            mediaplayer.setOnCompletionListener(object : MediaPlayer.OnCompletionListener {
-                override fun onCompletion(mediaPlayer: MediaPlayer) {
-                    mediaPlayer.reset()
-                    mediaPlayer.release()
-                }
-            })
-            mediaplayer.start()
         }
     }
 }
