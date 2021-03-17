8f73e95a5f5705ffb5ee84da866d15ebe580ba51
Mohit Kumar <mohitkumar@joshtalks.com>
2021-03-17 18:34:19 +0530

    Add favorite caller image binding adapter

--- app/src/main/java/com/joshtalks/joshskills/ui/inbox/adapter/InboxBindingAdapter.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/inbox/adapter/InboxBindingAdapter.kt
@@ -5,6 +5,7 @@ import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.integration.webp.decoder.WebpDrawable
 import com.bumptech.glide.integration.webp.decoder.WebpDrawableTransformation
+import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.load.MultiTransformation
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
@@ -54,4 +55,30 @@ object InboxBindingAdapter {
             .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
             .into(imageView)
     }
+
+
+    @BindingAdapter(value = ["favoriteCallerImage"], requireAll = false)
+    @JvmStatic
+    fun favoriteCallerImage(imageView: ImageView, url: String?) {
+        if (url.isNullOrEmpty()) {
+            imageView.setImageResource(R.drawable.ic_call_placeholder)
+            return
+        }
+        val requestOptions =
+            RequestOptions().placeholder(R.drawable.ic_call_placeholder)
+                .error(R.drawable.ic_call_placeholder)
+                .format(DecodeFormat.PREFER_RGB_565)
+                .disallowHardwareConfig().dontAnimate().encodeQuality(75)
+        Glide.with(AppObjectController.joshApplication)
+            .load(url)
+            .optionalTransform(
+                WebpDrawable::class.java,
+                WebpDrawableTransformation(CircleCrop())
+            )
+            .circleCrop()
+            .apply(requestOptions)
+            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+            .into(imageView)
+    }
+
 }
