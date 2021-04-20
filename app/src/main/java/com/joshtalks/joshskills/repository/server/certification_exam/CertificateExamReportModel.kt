992def0283bd567279bbf705560be241c7a84970
Mohit Kumar <mohitkumar@joshtalks.com>
2021-04-20 10:19:06 +0530

    add report id

--- app/src/main/java/com/joshtalks/joshskills/repository/server/certification_exam/CertificateExamReportModel.kt
+++ app/src/main/java/com/joshtalks/joshskills/repository/server/certification_exam/CertificateExamReportModel.kt
@@ -35,8 +35,9 @@ data class CertificateExamReportModel(
     @SerializedName("award_mentor")
     val awardMentor: Award?,
     @SerializedName("points")
-    val points: String?
-
+    val points: String?,
+    @SerializedName("report_id")
+    val reportId: Int = 0
 
 ) : Parcelable
 
