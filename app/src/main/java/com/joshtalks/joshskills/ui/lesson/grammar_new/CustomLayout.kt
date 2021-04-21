f095ca2fb9a8858695d0fffc7ec62cb057dac160
Sahil Chopra <sahilchopra0003@gmail.com>
2021-04-21 19:42:00 +0530

    Add pressable effect.

--- app/src/main/java/com/joshtalks/joshskills/ui/lesson/grammar_new/CustomLayout.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/lesson/grammar_new/CustomLayout.kt
@@ -3,6 +3,7 @@ package com.joshtalks.joshskills.ui.lesson.grammar_new
 import android.content.Context
 import android.view.View
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import com.joshtalks.joshskills.R
 import com.joshtalks.joshskills.repository.local.model.assessment.Choice
 import com.nex3z.flowlayout.FlowLayout
@@ -17,10 +18,12 @@ class CustomLayout(context: Context?) : FlowLayout(context) {
 
     fun removeViewCustomLayout(view: View, choice: Choice) {
         customWord = CustomWord(context, choice)
-        customWord!!.setBackgroundColor(resources.getColor(R.color.grey_61))
-        val params = LinearLayout.LayoutParams(view.width, view.height)
-        params.setMargins(20, 20, 20, 0)
-        customWord!!.layoutParams = params
+        customWord?.background =
+            ContextCompat.getDrawable(context, R.drawable.rounded_rectangle_grey)
+        customWord?.setTextColor(ContextCompat.getColor(context, R.color.light_shade_of_gray))
+//        val params = LinearLayout.LayoutParams(view.width, view.height)
+//        params.setMargins(20, 20, 20, 0)
+//        customWord!!.layoutParams = params
         removeView(view)
         addView(customWord, choice.sortOrder - 1)
     }
--- app/src/main/java/com/joshtalks/joshskills/ui/lesson/grammar_new/McqOptionView.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/lesson/grammar_new/McqOptionView.kt
@@ -1,12 +1,17 @@
 package com.joshtalks.joshskills.ui.lesson.grammar_new
 
 import android.content.Context
+import android.graphics.PorterDuff
+import android.view.Gravity
+import android.view.MotionEvent
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.appcompat.widget.AppCompatRadioButton
 import androidx.core.content.ContextCompat
+import com.github.mikephil.charting.utils.Utils
 import com.joshtalks.joshskills.R
 import com.joshtalks.joshskills.repository.local.model.assessment.Choice
+import kotlin.math.roundToInt
 
 class McqOptionView(context: Context) : AppCompatRadioButton(context) {
 
@@ -40,14 +45,20 @@ class McqOptionView(context: Context) : AppCompatRadioButton(context) {
 
         if (state == McqOptionState.UNSELECTED) {
             choice.isSelectedByUser = false
-            setTextColor(resources.getColor(R.color.black))
+            setTextColor(ContextCompat.getColor(context, R.color.grammar_black_text_color))
             background =
-                ContextCompat.getDrawable(getContext(), R.drawable.rounded_rectangle_with_border)
+                ContextCompat.getDrawable(
+                    getContext(),
+                    R.drawable.rounded_rectangle_with_grey_border
+                )
         } else {
             choice.isSelectedByUser = true
-            setTextColor(resources.getColor(R.color.button_color))
+            setTextColor(ContextCompat.getColor(context, R.color.grammar_button_color_blue))
             background =
-                ContextCompat.getDrawable(getContext(), R.drawable.primary_dark_rounded_bg)
+                ContextCompat.getDrawable(
+                    getContext(),
+                    R.drawable.rounded_rectangle_with_blue_border
+                )
         }
     }
 
@@ -56,13 +67,48 @@ class McqOptionView(context: Context) : AppCompatRadioButton(context) {
             ViewGroup.LayoutParams.MATCH_PARENT,
             ViewGroup.LayoutParams.WRAP_CONTENT
         )
+        layoutParams.gravity = Gravity.CENTER
         layoutParams.setMargins(20, 20, 20, 0)
         setLayoutParams(layoutParams)
+        gravity = Gravity.CENTER
         textAlignment = TEXT_ALIGNMENT_CENTER
         buttonDrawable = null
         textSize = 20f
+        setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    v.background = ContextCompat.getDrawable(
+                        context,
+                        R.drawable.rounded_rectangle_with_grey_border_pressed
+                    )
+                    val currentPadding = v.paddingTop
+                    v.setPadding(
+                        v.paddingLeft,
+                        currentPadding + Utils.convertDpToPixel(3f).roundToInt(),
+                        v.paddingRight,
+                        v.paddingBottom
+                    )
+                    //v.background.setColorFilter(-0x1f0b8adf, PorterDuff.Mode.SRC_ATOP)
+                    v.invalidate()
+                }
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                    v.background = ContextCompat.getDrawable(
+                        context,
+                        R.drawable.rounded_rectangle_with_grey_border
+                    )
+                    val currentPadding = v.paddingTop
+                    v.setPadding(
+                        v.paddingLeft,
+                        currentPadding - Utils.convertDpToPixel(3f).roundToInt(),
+                        v.paddingRight,
+                        v.paddingBottom
+                    )
+                    v.invalidate()
+                }
+            }
+            false
+        }
     }
-
 }
 
 enum class McqOptionState(state: String) {
