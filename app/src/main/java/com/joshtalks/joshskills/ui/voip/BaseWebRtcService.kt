f3bb96df8b07d92b49a16d1592912a85af12851b
Sahil Chopra <sahilchopra0003@gmail.com>
2021-05-07 13:14:56 +0530

    Merge fixes.

--- app/src/main/java/com/joshtalks/joshskills/ui/voip/BaseWebRtcService.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/voip/BaseWebRtcService.kt
@@ -3,23 +3,18 @@ package com.joshtalks.joshskills.ui.voip
 import android.annotation.SuppressLint
 import android.app.NotificationManager
 import android.app.Service
-import android.content.Intent
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
 import android.media.AudioAttributes
 import android.media.AudioFocusRequest
 import android.media.AudioManager
 import android.media.MediaPlayer
-import android.media.RingtoneManager // ktlint-disable no-wildcard-imports
+import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Build
 import android.os.VibrationEffect
 import android.os.Vibrator
 import com.joshtalks.joshskills.core.CALL_RINGTONE_NOT_MUTE
 import com.joshtalks.joshskills.core.JoshSkillExecutors
-import com.joshtalks.joshskills.core.PrefManager // ktlint-disable no-wildcard-imports
+import com.joshtalks.joshskills.core.PrefManager
 import com.joshtalks.joshskills.core.analytics.AppAnalytics
 import com.joshtalks.joshskills.ui.voip.util.WebRtcAudioManager
 import io.reactivex.disposables.CompositeDisposable
@@ -45,12 +40,12 @@ abstract class BaseWebRtcService : Service() { /*,SensorEventListener*/
         super.onCreate()
         mNotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager?
 
-            try {
-                joshAudioManager = WebRtcAudioManager(this)
-               /* cpuWakelock = (getSystemService(POWER_SERVICE) as PowerManager).newWakeLock(
-                PowerManager.PARTIAL_WAKE_LOCK, "joshtalsk"
-            )
-            cpuWakelock?.acquire(60 * 60 * 1000L *//*10 minutes*//*)*/
+        try {
+            joshAudioManager = WebRtcAudioManager(this)
+            /* cpuWakelock = (getSystemService(POWER_SERVICE) as PowerManager).newWakeLock(
+             PowerManager.PARTIAL_WAKE_LOCK, "joshtalsk"
+         )
+         cpuWakelock?.acquire(60 * 60 * 1000L *//*10 minutes*//*)*/
         } catch (ex: Exception) {
             ex.printStackTrace()
         }
@@ -223,31 +218,31 @@ abstract class BaseWebRtcService : Service() { /*,SensorEventListener*/
         }
     }
 
- /*   private fun showIncomingCallScreen(
-        data: HashMap<String, String?>,
-        autoPickupCall: Boolean = false
-    ) {
-        val callActivityIntent =
-            Intent(
-                this, WebRtcActivity::class.java
-            ).apply {
-                putExtra(CALL_TYPE, CallType.INCOMING)
-                putExtra(AUTO_PICKUP_CALL, autoPickupCall)
-                putExtra(CALL_USER_OBJ, data)
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            }
-        startActivity(callActivityIntent)
-    }
+    /*   private fun showIncomingCallScreen(
+           data: HashMap<String, String?>,
+           autoPickupCall: Boolean = false
+       ) {
+           val callActivityIntent =
+               Intent(
+                   this, WebRtcActivity::class.java
+               ).apply {
+                   putExtra(CALL_TYPE, CallType.INCOMING)
+                   putExtra(AUTO_PICKUP_CALL, autoPickupCall)
+                   putExtra(CALL_USER_OBJ, data)
+                   addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+               }
+           startActivity(callActivityIntent)
+       }
 
-    private fun isAppVisible(): Boolean {
-        return if (JoshApplication.isAppVisible || Build.VERSION.SDK_INT < Build.VERSION_CODES.Q)
-            return true
-        else
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && JoshApplication.isAppVisible.not()
-    }*/
+       private fun isAppVisible(): Boolean {
+           return if (JoshApplication.isAppVisible || Build.VERSION.SDK_INT < Build.VERSION_CODES.Q)
+               return true
+           else
+               Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && JoshApplication.isAppVisible.not()
+       }*/
 
     override fun onDestroy() {
         super.onDestroy()
-        removeSensor()
+        //removeSensor()
     }
 }
