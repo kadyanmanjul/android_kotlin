1aae7f5b7bdb7f97017fe48fdbd2a927ba6e6af1
Manjul Kadyan <manjulkadyan@joshtalks.com>
2021-08-03 11:47:24 +0530

    FreeTrialOnBoardActivity.kt,Add alert dialog on start free trial button

--- app/src/main/java/com/joshtalks/joshskills/ui/signup/FreeTrialOnBoardActivity.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/signup/FreeTrialOnBoardActivity.kt
@@ -1,12 +1,20 @@
 package com.joshtalks.joshskills.ui.signup
 
+import android.app.AlertDialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.textview.MaterialTextView
 import com.joshtalks.joshskills.R
 import com.joshtalks.joshskills.core.ApiCallStatus
+import com.joshtalks.joshskills.core.AppObjectController
 import com.joshtalks.joshskills.core.CoreJoshActivity
 import com.joshtalks.joshskills.core.analytics.AnalyticsEvent
 import com.joshtalks.joshskills.core.analytics.AppAnalytics
@@ -74,10 +82,31 @@ class FreeTrialOnBoardActivity : CoreJoshActivity() {
         }
     }
 
-    fun startTrial() {
-        if (Mentor.getInstance().getId().isNotEmpty()){
-            viewModel.startFreeTrial(Mentor.getInstance().getId())
+    fun showStartTrialPopup() {
+        val dialogBuilder: AlertDialog.Builder = AlertDialog.Builder(this)
+        val inflater = this.layoutInflater
+        val dialogView: View = inflater.inflate(R.layout.freetrial_alert_dialog, null)
+        dialogBuilder.setView(dialogView)
+
+        val alertDialog: AlertDialog = dialogBuilder.create()
+        val width = AppObjectController.screenWidth * .9
+        val height = ViewGroup.LayoutParams.WRAP_CONTENT
+        alertDialog.show()
+        alertDialog.window?.setLayout(width.toInt(), height)
+        alertDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        dialogView.findViewById<TextView>(R.id.e_g_motivat).text = getString(R.string.free_trial_dialog_desc).replace("\\n", "\n")
+        dialogView.findViewById<MaterialTextView>(R.id.yes).setOnClickListener {
+            if (Mentor.getInstance().getId().isNotEmpty()){
+                viewModel.startFreeTrial(Mentor.getInstance().getId())
+            }
+            alertDialog.dismiss()
         }
+
+        dialogView.findViewById<MaterialTextView>(R.id.cancel).setOnClickListener {
+            alertDialog.dismiss()
+        }
+
     }
 
     override fun onBackPressed() {
