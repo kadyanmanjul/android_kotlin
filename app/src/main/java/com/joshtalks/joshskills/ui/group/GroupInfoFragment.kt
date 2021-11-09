eacbae68b29929d608ac844c73e44d2c0db06a8d
saisukesh04 <sukesh.sai04@gmail.com>
2021-11-09 19:06:13 +0530

    Added fields to ViewModel

--- app/src/main/java/com/joshtalks/joshskills/ui/group/GroupInfoFragment.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/group/GroupInfoFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 
 import androidx.lifecycle.ViewModelProvider
 
 import com.joshtalks.joshskills.R
 import com.joshtalks.joshskills.base.BaseFragment
 import com.joshtalks.joshskills.databinding.GroupInfoFragmentBinding
+import com.joshtalks.joshskills.track.CONVERSATION_ID
 import com.joshtalks.joshskills.ui.group.viewmodels.GroupInfoViewModel
 
 private const val TAG = "GroupInfoFragment"
@@ -25,19 +27,31 @@ class GroupInfoFragment : BaseFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.group_info_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.group_info_fragment, container, false)
+        return binding.root
     }
 
     override fun initViewBinding() {
-        TODO("Not yet implemented")
+        binding.vm = vm
+        binding.executePendingBindings()
     }
 
-    override fun initViewState() {
-        TODO("Not yet implemented")
-    }
+    override fun initViewState() {}
 
     override fun setArguments() {
-        TODO("Not yet implemented")
+        arguments?.let {
+            vm.groupHeader.set(it.getString(GROUPS_TITLE, ""))
+            vm.hasJoinedGroup.set(it.getBoolean(HAS_JOINED_GROUP, false))
+            vm.groupSubHeader.set(it.getString(GROUPS_CHAT_SUB_TITLE, ""))
+            vm.groupId = it.getString(GROUPS_ID, "")
+            vm.imageUrl.set(it.getString(GROUPS_IMAGE, ""))
+            vm.groupCreatedAt.set(it.getString(GROUPS_CREATED_TIME, ""))
+            vm.groupCreator.set(it.getString(GROUPS_CREATOR, ""))
+            vm.conversationId = it.getString(CONVERSATION_ID, "") ?: ""
+        }
     }
 
+    override fun getConversationId(): String? {
+        return if(vm.conversationId.isBlank()) null else vm.conversationId
+    }
 }
