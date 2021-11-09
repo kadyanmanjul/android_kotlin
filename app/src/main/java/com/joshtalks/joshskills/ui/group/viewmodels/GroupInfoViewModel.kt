6251883d929c393fe6f23e42e5e2cb04b0d866a6
saisukesh04 <sukesh.sai04@gmail.com>
2021-11-09 19:06:13 +0530

    Added fields to ViewModel

--- app/src/main/java/com/joshtalks/joshskills/ui/group/viewmodels/GroupInfoViewModel.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/group/viewmodels/GroupInfoViewModel.kt
@@ -1,7 +1,25 @@
 package com.joshtalks.joshskills.ui.group.viewmodels
 
-import androidx.lifecycle.ViewModel
+import androidx.databinding.ObservableBoolean
+import androidx.databinding.ObservableField
+import com.joshtalks.joshskills.base.BaseViewModel
+import com.joshtalks.joshskills.constants.ON_BACK_PRESSED
+import com.joshtalks.joshskills.ui.group.repository.GroupRepository
 
-class GroupInfoViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class GroupInfoViewModel : BaseViewModel() {
+    val repository = GroupRepository()
+    val hasJoinedGroup = ObservableBoolean(false)
+    val groupHeader = ObservableField("")
+    val groupSubHeader = ObservableField("")
+    val imageUrl = ObservableField("")
+    val groupCreator = ObservableField("")
+    val groupCreatedAt = ObservableField("")
+    var conversationId: String = ""
+    val userOnlineCount = ObservableField("")
+    lateinit var groupId: String
+
+    fun onBackPress() {
+        message.what = ON_BACK_PRESSED
+        singleLiveEvent.value = message
+    }
 }
