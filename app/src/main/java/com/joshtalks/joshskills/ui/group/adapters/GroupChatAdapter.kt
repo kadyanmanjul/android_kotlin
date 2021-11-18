221a3779c2c8375b280091518603bfd5e1093c7f
saisukesh04 <sukesh.sai04@gmail.com>
2021-11-18 17:11:09 +0530

    Created groupChat adapter and viewholders

---
+++ app/src/main/java/com/joshtalks/joshskills/ui/group/adapters/GroupChatAdapter.kt
@@ -0,0 +1,99 @@
+package com.joshtalks.joshskills.ui.group.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+
+import com.joshtalks.joshskills.R
+import com.joshtalks.joshskills.databinding.GroupChatLeftMsgBinding
+import com.joshtalks.joshskills.databinding.GroupChatMetadataBinding
+import com.joshtalks.joshskills.databinding.GroupChatRightMsgBinding
+import com.joshtalks.joshskills.ui.group.model.GroupChatData
+import com.joshtalks.joshskills.ui.group.model.MessageType
+import com.joshtalks.joshskills.ui.group.viewholder.ChatViewHolder
+
+class GroupChatAdapter(diffCallback: DiffUtil.ItemCallback<GroupChatData>) :
+    PagingDataAdapter<GroupChatData, ChatViewHolder>(
+        diffCallback
+    ) {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ChatViewHolder {
+        return when (viewType) {
+            LEFT_TEXT_MESSAGE -> {
+                val view =
+                    setViewHolder<GroupChatLeftMsgBinding>(parent, R.layout.group_chat_left_msg)
+                LeftChatViewHolder(view)
+            }
+            RIGHT_TEXT_MESSAGE -> {
+                val view =
+                    setViewHolder<GroupChatRightMsgBinding>(parent, R.layout.group_chat_right_msg)
+                RightChatViewHolder(view)
+            }
+            META_DATA_MESSAGE, ERROR_TEXT_MESSAGE -> {
+                val view =
+                    setViewHolder<GroupChatMetadataBinding>(parent, R.layout.group_chat_metadata)
+                MetaChatViewHolder(view)
+            }
+            else -> {
+                val view =
+                    setViewHolder<GroupChatMetadataBinding>(parent, R.layout.group_chat_metadata)
+                MetaChatViewHolder(view)
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
+        getItem(position)?.let { holder.bindData(it) }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (getItem(position)?.getType()) {
+            MessageType.META_DATA -> META_DATA_MESSAGE
+            MessageType.SENT_MESSAGE -> RIGHT_TEXT_MESSAGE
+            MessageType.RECEIVED_MESSAGE -> LEFT_TEXT_MESSAGE
+            MessageType.DATA_ERROR -> ERROR_TEXT_MESSAGE
+            else -> ERROR_TEXT_MESSAGE
+        }
+    }
+
+    private fun <V : ViewDataBinding> setViewHolder(parent: ViewGroup, layoutId: Int): V {
+        return DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            layoutId,
+            parent,
+            false
+        )
+    }
+
+    inner class LeftChatViewHolder(val itemView: GroupChatLeftMsgBinding) :
+        ChatViewHolder(itemView) {
+        override fun bindData(groupChatData: GroupChatData) {
+
+        }
+    }
+
+    inner class RightChatViewHolder(val itemView: GroupChatRightMsgBinding) :
+        ChatViewHolder(itemView) {
+        override fun bindData(groupChatData: GroupChatData) {
+
+        }
+    }
+
+    inner class MetaChatViewHolder(val itemView: GroupChatMetadataBinding) :
+        ChatViewHolder(itemView) {
+        override fun bindData(groupChatData: GroupChatData) {
+
+        }
+    }
+}
+
+private const val LEFT_TEXT_MESSAGE = 1  //Message is received
+private const val RIGHT_TEXT_MESSAGE = 2 //Message is sent
+private const val META_DATA_MESSAGE = 3  //Message is of type metadata
+private const val ERROR_TEXT_MESSAGE = 0  //Message type is not known
