2ca4e38f2a0b43cbd3c27b2d04f4af02959447a1
saisukesh04 <sukesh.sai04@gmail.com>
2021-11-18 16:01:57 +0530

    PagingSource for group chat

---
+++ app/src/main/java/com/joshtalks/joshskills/ui/group/data/GroupChatPagingSource.kt
@@ -0,0 +1,25 @@
+package com.joshtalks.joshskills.ui.group.data
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.joshtalks.joshskills.ui.group.model.GroupChatData
+
+class GroupChatPagingSource(val apiService: GroupApiService, val channelId: String) :
+    PagingSource<Int, GroupChatData>() {
+    override fun getRefreshKey(state: PagingState<Int, GroupChatData>): Int? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, GroupChatData> {
+
+        val currentPageNo = params.key ?: 1
+        val responseData = apiService.getGroupChat(currentPageNo, channelId)
+
+        val data = responseData.chats?.map {
+            it as GroupChatData
+        }
+
+        return LoadResult.Page(data!!, if(currentPageNo == 1) null else currentPageNo-1, if(data.isEmpty()) null else currentPageNo+1)
+    }
+
+}
