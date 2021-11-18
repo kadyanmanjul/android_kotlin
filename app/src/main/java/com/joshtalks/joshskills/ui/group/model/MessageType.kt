e3c10b348b06752b09ef0e16170a9b1a52fd321a
saisukesh04 <sukesh.sai04@gmail.com>
2021-11-18 15:58:27 +0530

    ChatResponse and MessageTypes

---
+++ app/src/main/java/com/joshtalks/joshskills/ui/group/model/MessageType.kt
@@ -0,0 +1,8 @@
+package com.joshtalks.joshskills.ui.group.model
+
+enum class MessageType (val type: String) {
+    SENT_MESSAGE("SENT_MESSAGE"),
+    RECEIVED_MESSAGE("RECEIVED_MESSAGE"),
+    META_DATA("META_DATA"),
+    DATA_ERROR("DATA_ERROR")
+}
