ccb5ef3e314500deec7898aa1fe94f12cb6c6070
saisukesh04 <sukesh.sai04@gmail.com>
2021-11-19 16:16:23 +0530

    Tooltip for groupList

--- app/src/main/java/com/joshtalks/joshskills/ui/group/GroupListFragment.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/group/GroupListFragment.kt
@@ -4,22 +4,29 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+
 import androidx.appcompat.widget.PopupMenu
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+
 import com.joshtalks.joshskills.R
 import com.joshtalks.joshskills.base.BaseFragment
 import com.joshtalks.joshskills.constants.OPEN_POPUP_MENU
+import com.joshtalks.joshskills.core.HAS_SEEN_GROUP_TOOLTIP
+import com.joshtalks.joshskills.core.PrefManager
 import com.joshtalks.joshskills.databinding.FragmentGroupListBinding
-import com.joshtalks.joshskills.ui.group.analytics.GroupAnalytics
 import com.joshtalks.joshskills.ui.group.analytics.GroupAnalytics.Event.*
 import com.joshtalks.joshskills.ui.group.viewmodels.JoshGroupViewModel
+
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.distinctUntilChanged
 
 private const val TAG = "GroupListFragment"
+
 class GroupListFragment : BaseFragment() {
     lateinit var binding: FragmentGroupListBinding
     val vm by lazy {
@@ -41,7 +48,25 @@ class GroupListFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_group_list, container, false)
+        initTooltip()
         return binding.root
+    }
+
+    private fun initTooltip() {
+        if (!PrefManager.getBoolValue(HAS_SEEN_GROUP_TOOLTIP)) {
+            binding.animLayout.visibility = VISIBLE
+            binding.overlayGroupTooltip.visibility = VISIBLE
+            binding.overlayLayout.visibility = VISIBLE
+
+            PrefManager.put(HAS_SEEN_GROUP_TOOLTIP, true)
+
+            binding.overlayLayout.setOnClickListener {
+                binding.animLayout.visibility = GONE
+                binding.overlayGroupTooltip.visibility = GONE
+                binding.overlayLayout.visibility = GONE
+                binding.overlayLayout.setOnClickListener(null)
+            }
+        }
     }
 
     override fun initViewBinding() {
@@ -52,12 +77,12 @@ class GroupListFragment : BaseFragment() {
     }
 
     override fun getConversationId(): String? {
-        return if(vm.conversationId.isBlank()) null else vm.conversationId
+        return if (vm.conversationId.isBlank()) null else vm.conversationId
     }
 
     override fun initViewState() {
         liveData.observe(viewLifecycleOwner) {
-            when(it.what) {
+            when (it.what) {
                 OPEN_POPUP_MENU -> {
                     openPopupMenu()
                 }
@@ -67,7 +92,7 @@ class GroupListFragment : BaseFragment() {
 
     override fun onStart() {
         super.onStart()
-        if(vm.shouldRefreshGroupList) {
+        if (vm.shouldRefreshGroupList) {
             vm.shouldRefreshGroupList = false
             vm.adapter.refresh()
         }
@@ -76,7 +101,11 @@ class GroupListFragment : BaseFragment() {
     override fun setArguments() {}
 
     private fun openPopupMenu() {
-        val popupMenu = PopupMenu(requireContext(), binding.groupAppBar.secondIconImageView, R.style.setting_menu_style)
+        val popupMenu = PopupMenu(
+            requireContext(),
+            binding.groupAppBar.secondIconImageView,
+            R.style.setting_menu_style
+        )
         popupMenu.inflate(R.menu.groups_menu)
         popupMenu.setOnMenuItemClickListener {
             popupMenu.dismiss()
