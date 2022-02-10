f40becd2245500eded2040cc5f6b765a5decd103
Sagar Jain <sagar@joshtalks.com>
2022-02-10 11:33:00 +0530

    Create Fpp List UI Create Recent Call List UI Modify profile UI

---
+++ app/src/main/java/com/joshtalks/joshskills/ui/fpp/RecentCallActivity.kt
@@ -0,0 +1,16 @@
+package com.joshtalks.joshskills.ui.fpp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.joshtalks.joshskills.R
+
+class RecentCallActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recent_call)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+}
