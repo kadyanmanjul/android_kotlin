38a19ff9f42d69cda11dd797b6a18af3f6e95b32
yashkasera <yashkasera@icloud.com>
2022-03-03 21:07:10 +0530

    Initial Special Practice code

---
+++ app/src/main/java/com/joshtalks/joshskills/ui/special_practice/RecordVideoActivity.kt
@@ -0,0 +1,12 @@
+package com.joshtalks.joshskills.ui.special_practice
+
+import android.os.Bundle
+import com.joshtalks.joshskills.R
+import com.joshtalks.joshskills.core.CoreJoshActivity
+
+class RecordVideoActivity : CoreJoshActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record_video)
+    }
+}
