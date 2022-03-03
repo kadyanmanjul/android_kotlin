34c5acede28f9a1c8fabc5c7e91eec8044ffafde
namanmahendra01 <namanmahendra01@gmail.com>
2022-03-03 17:56:01 +0530

    PSTN INDICATOR MODULE

---
+++ app/src/main/java/com/joshtalks/joshskills/ui/voip/pstn_call_indicator/PSTNCallState.kt
@@ -0,0 +1,76 @@
+package com.joshtalks.joshskills.ui.voip.pstn_call_indicator
+
+import android.content.BroadcastReceiver
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.telephony.PhoneStateListener
+import android.telephony.TelephonyManager
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
+
+/** <add receiver> in manifest
+
+<receiver android:name=".pstn_call_indicator.PhoneCallReceiver"
+android:exported="true"
+android:enabled="true">
+<intent-filter>
+<action android:name="android.intent.action.PHONE_STATE"/>
+</intent-filter>
+<intent-filter>
+<action android:name="android.intent.action.NEW_OUTGOING_CALL"/>
+</intent-filter>
+</receiver>
+ */
+class PSTNCallState : BroadcastReceiver(){
+
+    companion object {
+        val pstnFlow = MutableSharedFlow<PSTNListener.PSTNState>()
+    }
+    override fun onReceive(context: Context, intent: Intent) {
+        try {
+            getCallingState(context, intent)
+        } catch (ex: Exception) {
+            try {
+            } catch (e: Exception) {
+            }
+        }
+
+    }
+
+    private fun getCallingState(context: Context, intent: Intent) {
+
+        val telephony = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        val customPhoneListener = PSTNListener()
+        telephony.listen(customPhoneListener, PhoneStateListener.LISTEN_CALL_STATE)
+        val stateStr = intent.extras!!.getString(TelephonyManager.EXTRA_STATE)
+        var state = 0
+        when (stateStr) {
+            TelephonyManager.EXTRA_STATE_IDLE -> {
+                state = TelephonyManager.CALL_STATE_IDLE
+                CoroutineScope(Dispatchers.IO).launch {
+                    pstnFlow.emit(PSTNListener.PSTNState.Idle)
+                }
+                Log.d(TAG, "getCallingState: Idle $state")
+            }
+            TelephonyManager.EXTRA_STATE_OFFHOOK -> {
+                state = TelephonyManager.CALL_STATE_OFFHOOK
+                CoroutineScope(Dispatchers.IO).launch {
+                    pstnFlow.emit(PSTNListener.PSTNState.OnCall)
+                }
+                Log.d(TAG, "getCallingState:OffHook  $state")
+            }
+            TelephonyManager.EXTRA_STATE_RINGING -> {
+                state = TelephonyManager.CALL_STATE_RINGING
+                CoroutineScope(Dispatchers.IO).launch {
+                    pstnFlow.emit(PSTNListener.PSTNState.Ringing)
+                }
+                Log.d(TAG, "getCallingState: Ringing $state")
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/joshtalks/joshskills/ui/voip/pstn_call_indicator/PSTNListener.kt
@@ -0,0 +1,14 @@
+package com.joshtalks.joshskills.ui.voip.pstn_call_indicator
+import android.telephony.PhoneStateListener
+
+class PSTNListener:PhoneStateListener() {
+
+    override fun onCallStateChanged(state: Int, phoneNumber: String?) {
+        super.onCallStateChanged(state, phoneNumber)
+    }
+    sealed class PSTNState(){
+        object Idle: PSTNState()
+        object Ringing: PSTNState()
+        object OnCall: PSTNState()
+    }
+}
