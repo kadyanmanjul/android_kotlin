77ee8805db867837b0b132fb057fc6443ffddd1e
namanmahendra01 <namanmahendra01@gmail.com>
2022-03-04 15:57:15 +0530

    MinorFix:UserPresence polishing and setting some values

--- app/src/main/java/com/joshtalks/joshskills/ui/voip/user_presence/UserPresence.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/voip/user_presence/UserPresence.kt
@@ -3,9 +3,11 @@ package com.joshtalks.joshskills.ui.voip.user_presence
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
-class UserPresence():UserPresenceInterface {
-    private val userMap:HashMap<String,Any> = HashMap<String,Any>()
+class UserPresence :UserPresenceInterface {
+    private val userMap:HashMap<String,Any> = HashMap()
     private val pathString:String="OnlineStatus"
+    private var status:Boolean=false
+
     companion object {
         var database: DatabaseReference?=null
     }
@@ -19,8 +21,16 @@ class UserPresence():UserPresenceInterface {
     }
 
     override fun setUserPresenceInDB(key: String, value: PresenceStatus) {
+        status = when(value){
+            PresenceStatus.Online->{
+                true
+            }
+            PresenceStatus.Offline->{
+                false
+            }
+        }
         getFirebaseDBReference()
-        userMap[key] = value
+        userMap[key] = status
         database?.child(pathString)?.updateChildren(userMap)
     }
 
