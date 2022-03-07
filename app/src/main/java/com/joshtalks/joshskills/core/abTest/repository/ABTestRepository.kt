8f6527f26eb7d7016891f5c9e2a5169e0bcf27e7
Manjul Kadyan <manjulkadyan@joshtalks.com>
2022-03-07 21:25:55 +0530

    ABTestActivity.kt,Integrate ab test API's for ab test campaigns.

---
+++ app/src/main/java/com/joshtalks/joshskills/core/abTest/repository/ABTestRepository.kt
@@ -0,0 +1,63 @@
+package com.joshtalks.joshskills.ui.group.repository
+
+import android.util.Log
+import com.joshtalks.joshskills.core.AppObjectController
+import com.joshtalks.joshskills.core.abTest.ABTestCampaignData
+import com.joshtalks.joshskills.util.showAppropriateMsg
+
+private const val TAG = "ABTestRepository"
+
+class ABTestRepository {
+
+    private val apiService by lazy { AppObjectController.abTestNetworkService }
+    private val database = AppObjectController.appDatabase.abCapmaignDao()
+
+    suspend fun getCampaignData(campaign: String): ABTestCampaignData? {
+
+        if (database.getABTestCampaign(campaign) != null) {
+            return database.getABTestCampaign(campaign)!!
+        } else {
+            try {
+                val apiResponse = apiService.getCampaignData(campaign)
+                if (apiResponse.isSuccessful && apiResponse.body() != null) {
+                    database.insertCampaign(apiResponse.body()!!)
+                    return database.getABTestCampaign(campaign)!!
+                }
+            } catch (ex: Throwable) {
+                ex.showAppropriateMsg()
+            }
+            return null
+        }
+    }
+
+    suspend fun updateAllCampaigns(list: List<String>) {
+        try {
+            database.deleteAllCampaigns()
+            Log.d("Manjul", "updateAllCampaigns() called with: list = ${list.joinToString(",")}")
+            val apiResponse = apiService.getAllCampaigns(list.joinToString(","))
+            if (apiResponse.isSuccessful && apiResponse.body() != null) {
+                database.insertCampaigns(apiResponse.body()!!)
+            }
+        } catch (ex: Throwable) {
+            ex.showAppropriateMsg()
+        }
+    }
+
+    suspend fun getAllCampaigns(): List<ABTestCampaignData>? {
+        try {
+            return database.getAllABTestCampaigns()
+        } catch (ex: Throwable) {
+            ex.showAppropriateMsg()
+        }
+        return null
+    }
+
+    suspend fun postGoal(goal: String) {
+        try {
+            apiService.postGoalData(mapOf("goal_key" to goal))
+        } catch (ex: Throwable) {
+            ex.showAppropriateMsg()
+        }
+    }
+
+}
