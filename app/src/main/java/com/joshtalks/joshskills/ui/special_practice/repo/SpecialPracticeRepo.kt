875b8d8ad19a99fab5fdafbf47c862b621d89380
sagar <sagar@joshtalks.com>
2022-03-29 14:43:08 +0530

    Code refactor 1. Unnecessary code remove

--- app/src/main/java/com/joshtalks/joshskills/ui/special_practice/repo/SpecialPracticeRepo.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/special_practice/repo/SpecialPracticeRepo.kt
@@ -1,8 +1,12 @@
 package com.joshtalks.joshskills.ui.special_practice.repo
 
 import com.joshtalks.joshskills.core.AppObjectController
+import com.joshtalks.joshskills.ui.special_practice.model.SaveVideoModel
 
 class SpecialPracticeRepo {
     suspend fun getSpecialData(params: HashMap<String, String>) =
         AppObjectController.commonNetworkService.getSpecialPracticeDetails(params)
+
+    suspend fun saveRecordedVideo(params: SaveVideoModel) =
+        AppObjectController.commonNetworkService.saveVideoOnServer(params)
 }
