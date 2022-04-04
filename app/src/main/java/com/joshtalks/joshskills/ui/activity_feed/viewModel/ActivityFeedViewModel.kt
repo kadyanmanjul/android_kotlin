50e84218de5f626dbc6c2100e73de35ca7ecf972
moheeeetgupta <mohitgupta@joshtalks.com>
2022-04-04 16:18:31 +0530

    Bug Fixing

--- app/src/main/java/com/joshtalks/joshskills/ui/activity_feed/viewModel/ActivityFeedViewModel.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/activity_feed/viewModel/ActivityFeedViewModel.kt
@@ -4,8 +4,12 @@ import android.annotation.SuppressLint
 import android.os.Handler
 import android.provider.Contacts
 import android.view.View
+import android.widget.SeekBar
 import androidx.databinding.ObservableBoolean
 import androidx.databinding.ObservableField
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 import androidx.lifecycle.viewModelScope
 import com.clevertap.android.sdk.Utils.runOnUiThread
 import com.joshtalks.joshskills.base.BaseViewModel
@@ -15,15 +19,18 @@ import com.joshtalks.joshskills.ui.activity_feed.ActivityFeedListAdapter
 import com.joshtalks.joshskills.ui.activity_feed.model.ActivityFeedResponse
 import com.joshtalks.joshskills.ui.activity_feed.repository.ActivityFeedRepository
 import com.joshtalks.joshskills.ui.activity_feed.utils.*
+import com.joshtalks.joshskills.util.ExoAudioPlayer2
 import com.joshtalks.joshskills.util.showAppropriateMsg
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import me.zhanghai.android.materialplaypausedrawable.MaterialPlayPauseButton
+import me.zhanghai.android.materialplaypausedrawable.MaterialPlayPauseDrawable
 import java.sql.Timestamp
 
-class ActivityFeedViewModel : BaseViewModel() {
-    val firebaseRepository = ActivityFeedRepository()
+class ActivityFeedViewModel : BaseViewModel(), LifecycleObserver {
+    val activityFeedRepository by lazy{ ActivityFeedRepository() }
 
     val adapter = ActivityFeedListAdapter()
     val isScrollToEndButtonVisible = ObservableBoolean(false)
@@ -32,80 +39,14 @@ class ActivityFeedViewModel : BaseViewModel() {
     var startTime = System.currentTimeMillis()
     var impressionId = ObservableField(EMPTY)
 
-
-//    fun getFeed() {
-//        viewModelScope.launch(Dispatchers.IO) {
-//            try {
-//                fetchingAllFeed.set(true)
-//                val collectionRef = firebaseRepository.getActivityFeed()
-//                collectionRef.addSnapshotListener { value, e ->
-//                    if (e != null) {
-//                        return@addSnapshotListener
-//                    }
-//                    if (FirstTimeUser.flag) {
-//                        for (doc in value!!.documentChanges) {
-//                            if (doc.type == DocumentChange.Type.ADDED) {
-//                                if (localFlag) {
-//                                    val timeGap = (System.currentTimeMillis() - feedTime).div(1000)
-//                                    if (timeGap >= 0.5) {
-//                                        fetchingAllFeed.set(false)
-//                                        currentFeed = doc.document.toObject()
-//                                        currentFeed.photoUrl =
-//                                            doc.document.get("photo_url").toString()
-//                                        if(doc.document.get("event_id").toString()!="null") {
-//                                            currentFeed.eventId =
-//                                                doc.document.get("event_id").toString().toInt()
-//                                        }
-//                                        currentFeed.mediaUrl =
-//                                            doc.document.get("media_url").toString()
-//                                        currentFeed.mentorId =
-//                                            doc.document.get("mentor_id").toString()
-//                                        if(doc.document.get("media_duration").toString()!="null") {
-//                                            currentFeed.duration =
-//                                                doc.document.get("media_duration").toString()
-//                                                    .toInt()
-//                                        }
-//                                        feedTime = System.currentTimeMillis()
-//                                        currentFeed?.let { adapter.items.add(0, it) }
-//                                        adapter.notifyItemInserted(0)
-//                                        message.what= ON_ITEM_ADDED
-//                                        singleLiveEvent.value=message
-//                                    }
-//                                } else {
-//                                    fetchingAllFeed.set(false)
-//                                    currentFeed = doc.document.toObject()
-//                                    currentFeed.photoUrl =
-//                                        doc.document.get("photo_url").toString()
-//                                    if(doc.document.get("event_id").toString()!="null") {
-//                                        currentFeed.eventId =
-//                                            doc.document.get("event_id").toString().toInt()
-//                                    }
-//                                    currentFeed.mediaUrl =
-//                                        doc.document.get("media_url").toString()
-//                                    currentFeed.mentorId =
-//                                        doc.document.get("mentor_id").toString()
-//                                    if(doc.document.get("media_duration").toString()!="null") {
-//                                        currentFeed.duration =
-//                                            doc.document.get("media_duration").toString()
-//                                                .toInt()
-//                                    }
-//                                    currentFeed?.let { adapter.items.add(0, it) }
-//                                    adapter.notifyItemInserted(0)
-//                                }
-//
-//                            }
-//                        }
-//                    }
-//                    localFlag = true
-//                    FirstTimeUser.flag = true
-//                }
-//            } catch (ex: Throwable) {
-//                ex.showAppropriateMsg()
-//                fetchingAllFeed.set(false)
-//            }
-//        }
-//
-//    }
+    private lateinit var playPauseButton: MaterialPlayPauseButton
+    private lateinit var seekbar: SeekBar
+    val exoAudioManager by lazy { ExoAudioPlayer2() }
+    private lateinit var prevPlayPauseButton: MaterialPlayPauseButton
+    private var id: String = EMPTY
+    private var url: String = EMPTY
+    private var lastPosition: Long = 0L
+    private var duration: Int = 0
 
     fun getActivityFeed(timestamp: String) {
         viewModelScope.launch(Dispatchers.IO) {
@@ -114,21 +55,20 @@ class ActivityFeedViewModel : BaseViewModel() {
                     fetchingAllFeed.set(true)
                 }
                 val response =
-                    AppObjectController.commonNetworkService.getActivityFeedData(timestamp)
+                    activityFeedRepository.getActivityFeedData(timestamp)
                 if (response.isSuccessful) {
                     response.body()?.let {
                         fetchingAllFeed.set(false)
                         impressionId.set(response.body()?.impressionId)
                         it.activityList?.forEach {currentFeed->
                             addItem(currentFeed)
-                            delay(1000)
+                            delay(60000L.div(it.activityList.size))
                         }
                         it.activityList?.let{
                             if(it.size==0){
-                                delay(60L)
+                                delay(60000L)
                             }
                         }
-
                         response.body()?.timestamp?.let { getActivityFeed(it) }
                     }
 
@@ -155,7 +95,7 @@ class ActivityFeedViewModel : BaseViewModel() {
                 if (impressionId.isBlank())
                     return@launch
 
-                AppObjectController.commonNetworkService.engageActivityFeedTime(
+                activityFeedRepository.engageActivityFeedTime(
                     impressionId,
                     mapOf("time_spent" to startTime)
                 )
@@ -166,21 +106,101 @@ class ActivityFeedViewModel : BaseViewModel() {
         }
     }
 
-    val onItemClick: (ActivityFeedResponse, Int) -> Unit = { it, type ->
+    val onItemClick: (ActivityFeedResponse, Int, SeekBar?, MaterialPlayPauseButton?, ActivityFeedListAdapter.ViewHolder?) -> Unit = { activityFeedResponse, type,seekBar,playPauseBtn,viewHolder ->
         when (type) {
             OPEN_FEED_USER_PROFILE -> {
                 message.what = OPEN_FEED_USER_PROFILE
-                message.obj = it
+                message.obj = activityFeedResponse
                 singleLiveEvent.value = message
             }
             OPEN_PROFILE_IMAGE_FRAGMENT -> {
                 message.what = OPEN_PROFILE_IMAGE_FRAGMENT
-                message.obj = it
+                message.obj = activityFeedResponse
                 singleLiveEvent.value = message
+            }
+            PLAY_AUDIO->{
+                seekbar = seekBar!!
+                playPauseButton = playPauseBtn!!
+                seekbar.progress = 0
+                id = System.currentTimeMillis().toString()
+                this.url = activityFeedResponse.mediaUrl.toString()
+                this.duration = activityFeedResponse.duration
+                seekbar.max = duration
+                if (playPauseButton.state == MaterialPlayPauseDrawable.State.Play) {
+                    if(exoAudioManager.isPlaying()){
+                        prevPlayPauseButton.state = MaterialPlayPauseDrawable.State.Play
+                    }
+                    prevPlayPauseButton=playPauseButton
+                    removeSeekbarListener()
+                    if (viewHolder != null) {
+                        addListener(viewHolder)
+                    }
+                    initAndPlay()
+                } else {
+                    playPauseButton.state = MaterialPlayPauseDrawable.State.Play
+                    onPausePlayer()
+                }
+            }
+            ON_AUDIO_COMPLETE->{
+                playPauseButton.state = MaterialPlayPauseDrawable.State.Play
+                playPauseButton.jumpToState(MaterialPlayPauseDrawable.State.Play)
+                seekbar.progress = 0
+                exoAudioManager?.seekTo(0)
+                exoAudioManager?.onPause()
+                exoAudioManager?.setProgressUpdateListener(null)
             }
         }
     }
+    private fun removeSeekbarListener() {
+        seekbar.setOnSeekBarChangeListener(null)
+        exoAudioManager?.playerListener = null
+        exoAudioManager?.setProgressUpdateListener(
+            null
+        )
+    }
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun onPausePlayer() {
+        if(exoAudioManager.isPlaying()) {
+            exoAudioManager?.onPause()
+            playPauseButton.state = MaterialPlayPauseDrawable.State.Play
+        }
 
+    }
+
+    private fun addListener(viewHolder: ActivityFeedListAdapter.ViewHolder) {
+        seekbar.setOnSeekBarChangeListener(null)
+        seekbar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                var userSelectedPosition = 0
+                override fun onStartTrackingTouch(seekBar: SeekBar) {
+                }
+
+                override fun onProgressChanged(
+                    seekBar: SeekBar,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    if (fromUser) {
+                        userSelectedPosition = progress
+                    }
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar) {
+                    exoAudioManager?.seekTo(userSelectedPosition.toLong())
+                }
+            })
+        exoAudioManager?.playerListener = viewHolder
+        exoAudioManager?.setProgressUpdateListener(viewHolder)
+    }
+
+    private fun initAndPlay() {
+        url.let {
+            exoAudioManager?.play(it, id, lastPosition)
+            seekbar.progress = lastPosition.toInt()
+            playPauseButton.state = MaterialPlayPauseDrawable.State.Pause
+
+        }
+    }
     fun onBackPress(view: View) {
         saveEngageTime()
     }
