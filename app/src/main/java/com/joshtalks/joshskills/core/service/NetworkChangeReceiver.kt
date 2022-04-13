e8b963308839d2c68149823f62d929c50992ddb5
saisukesh04 <sukesh.sai04@gmail.com>
2022-04-13 11:15:29 +0530

    Broadcast event logging in database

--- app/src/main/java/com/joshtalks/joshskills/core/service/NetworkChangeReceiver.kt
+++ app/src/main/java/com/joshtalks/joshskills/core/service/NetworkChangeReceiver.kt
@@ -3,14 +3,24 @@ package com.joshtalks.joshskills.core.service
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.joshtalks.joshskills.core.JoshApplication
-import com.joshtalks.joshskills.core.Utils
+import com.joshtalks.joshskills.core.AppObjectController
+import com.joshtalks.joshskills.repository.local.model.Mentor
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class NetworkChangeReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         try {
-            if (Utils.isInternetAvailable() && JoshApplication.isAppVisible) {
-//                WorkManagerAdmin.userActiveStatusWorker(JoshApplication.isAppVisible)
+            CoroutineScope(Dispatchers.IO).launch {
+                Timber.tag("ReceiverCheck").e("${intent.action} : ${intent.dataString}")
+
+                val request = mapOf(
+                    Pair("mentor_id", Mentor.getInstance().getId()),
+                    Pair("event_name", intent.action)
+                )
+                AppObjectController.commonNetworkService.saveBroadcastEvent(request)
             }
         } catch (ex: Throwable) {
             ex.printStackTrace()
