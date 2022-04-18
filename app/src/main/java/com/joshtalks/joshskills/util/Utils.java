f8dafdc90dfed6f961e46f5823f98043eae4dee2
saisukesh04 <sukesh.sai04@gmail.com>
2022-04-18 17:45:24 +0530

    Full working of normal MoengagePush

--- app/src/main/java/com/joshtalks/joshskills/util/Utils.java
+++ app/src/main/java/com/joshtalks/joshskills/util/Utils.java
@@ -1,8 +1,14 @@
 package com.joshtalks.joshskills.util;
 
 import android.text.format.DateFormat;
+
+import org.json.JSONObject;
+
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Locale;
+import java.util.Map;
 
 public class Utils {
     public static String DATE_FORMAT = "yyyyMMdd_HHmmsyyyyMMdd_HHmmss";
@@ -19,4 +25,18 @@ public class Utils {
         return DateFormat.format("dd/MM/yyyy", var2).toString();
     }
 
+    public static Map<String, String> jsonToMap(JSONObject json) {
+        if (json == null || json.length() == 0) return new HashMap<>();
+        Map<String, String> map = new HashMap<>(json.length());
+        Iterator<String> iterator = json.keys();
+        try {
+            while (iterator.hasNext()) {
+                String key = iterator.next();
+                map.put(key, json.getString(key));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
 }
