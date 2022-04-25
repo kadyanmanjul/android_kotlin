a0435aabbbcd6d53798b1911911bb27bcbed6dea
Yashendra Shukla <yashendrashukla@joshtalks.com>
2022-04-25 17:08:11 +0530

    search feature updates

--- badebhaiya/src/main/java/com/joshtalks/badebhaiya/SearchAdapter.kt
+++ badebhaiya/src/main/java/com/joshtalks/badebhaiya/SearchAdapter.kt
@@ -4,23 +4,37 @@ import android.app.PendingIntent.getActivity
 import android.content.Context
 import android.os.Bundle
 import android.os.Message
+import android.provider.Settings.Global.getString
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.os.persistableBundleOf
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.joshtalks.badebhaiya.core.showToast
+import com.joshtalks.badebhaiya.databinding.ActivityProfileBinding
 import com.joshtalks.badebhaiya.databinding.LiSearchEventBinding
 import com.joshtalks.badebhaiya.feed.model.SearchRoomsResponse
 import com.joshtalks.badebhaiya.feed.model.Users
 import com.joshtalks.badebhaiya.liveroom.OPEN_PROFILE
 import com.joshtalks.badebhaiya.profile.ProfileActivity
+import com.joshtalks.badebhaiya.profile.ProfileViewModel
+import com.joshtalks.badebhaiya.profile.request.FollowRequest
 import com.joshtalks.badebhaiya.repository.model.User
+import com.joshtalks.badebhaiya.repository.service.RetrofitInstance
 import com.joshtalks.badebhaiya.utils.setImage
 import com.joshtalks.badebhaiya.utils.setUserInitialInRect
+import kotlinx.android.synthetic.main.activity_profile.view.*
+import kotlinx.android.synthetic.main.activity_profile.view.btnFollow
+import kotlinx.android.synthetic.main.li_search_event.*
+import kotlinx.android.synthetic.main.li_search_event.view.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import java.security.AccessController.getContext
 
 
@@ -43,23 +57,24 @@ class SearchAdapter(private val searchResult: List<Users>): ListAdapter<SearchRo
 
     }
 
-    inner class SearchViewHolder( var item:LiSearchEventBinding):
-        RecyclerView.ViewHolder(item.root) {
-            fun onBind(user:SearchRoomsResponse)
-            {
-                item.roomData=user.users
-                val name =user.users.full_name
-                val bio = user.users.bio
-                var profilePic = user.users.profilePic
-                item.ivProfilePic.setImage(profilePic)
-                item.userName.setText(name)
-                item.tvProfileBio.setText(bio)
+    val speakerFollowed = MutableLiveData(false)
 
-            }
+    inner class SearchViewHolder(var item: LiSearchEventBinding) :
+        RecyclerView.ViewHolder(item.root) {
+        fun onBind(user: SearchRoomsResponse) {
+            item.roomData = user.users
+            val name = user.users.full_name
+            val bio = user.users.bio
+            var profilePic = user.users.profilePic
+            item.ivProfilePic.setImage(profilePic)
+            item.userName.setText(name)
+            item.tvProfileBio.setText(bio)
+
+        }
 
     }
 
-    override fun getItemCount()=searchResult.size
+    override fun getItemCount(): Int = searchResult?.size?:0
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
@@ -69,8 +84,12 @@ class SearchAdapter(private val searchResult: List<Users>): ListAdapter<SearchRo
             parent,
             false
         )
+//        view.btnFollow.setOnClickListener{
+//            showToast("hum bhi bana lenge ")
+//        }
         return SearchViewHolder(view)
     }
+
     var message = Message()
 
     var singleLiveEvent: MutableLiveData<Message> = MutableLiveData()
@@ -85,28 +104,96 @@ class SearchAdapter(private val searchResult: List<Users>): ListAdapter<SearchRo
         singleLiveEvent.postValue(message)
     }
 
+//     fun addList(user:List<Users>){
+//        searchResult=user
+//         notifyDataSetChanged()
+//
+//    }
+
     override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
 
-        holder.item.tvProfileBio.text=searchResult[position].bio
-        holder.item.userName.text=searchResult[position].full_name
-        //holder.item.btnFollow.visibility=viewModel.isBadeBhaiyaSpeaker && !viewModel.isSelfProfile) ? View.VISIBLE : View.GONE
-        //holder.item.btnFollow.visibility=(searchResult[position].is_speaker_followed)
-        holder.item.btnFollow.setOnClickListener{
-            showToast("followed")
-        }
-        holder.item.user.setOnClickListener{
-            ProfileActivity.openProfileActivity(holder.item.user.context,searchResult[position].user_id)
+        searchResult.let {
+                searchResult->
+            holder.item.tvProfileBio.text = searchResult[position].bio
+            holder.item.userName.text = searchResult[position].full_name
+            //holder.item.btnFollow.visibility=viewModel.isBadeBhaiyaSpeaker && !viewModel.isSelfProfile) ? View.VISIBLE : View.GONE
+            //holder.item.btnFollow.visibility=(searchResult[position].is_speaker_followed)
+    //            holder.item.btnFollow.setOnClickListener {
+    //                showToast("followed")
+    //            }
+            holder.item.user.setOnClickListener {
+                ProfileActivity.openProfileActivity(
+                    holder.item.user.context,
+                    searchResult[position].user_id
+                )
             }
 
+            if (searchResult[position].profilePic.isNullOrEmpty().not())
+                Glide.with(holder.itemView.getContext())
+                    .load(searchResult[position].profilePic)
+                    .into(holder.item.ivProfilePic)
+            else
+                holder.item.ivProfilePic.setUserInitialInRect(searchResult[position].short_name, 24)
 
-        if(searchResult[position].profilePic.isNullOrEmpty().not())
-        Glide.with(holder.itemView.getContext())
-            .load(searchResult[position].profilePic)
-            .into(holder.item.ivProfilePic)
-        else
-            holder.item.ivProfilePic.setUserInitialInRect(searchResult[position].short_name,24)
+            if (searchResult[position].is_speaker_followed) {
+                holder.item.user.apply {
+                    holder.item.user.btnFollow.text = "Following"
+                    btnFollow.setTextColor(resources.getColor(R.color.white))
+                    btnFollow.background = AppCompatResources.getDrawable(
+                        holder.item.user.btnFollow.context,
+                        R.drawable.following_button_background
+                    )
+                }
+            } else
+                holder.item.user.apply {
+                    holder.item.user.btnFollow.text = "Follow"
+                    btnFollow.setTextColor(resources.getColor(R.color.follow_button_stroke))
+                    btnFollow.background = AppCompatResources.getDrawable(
+                        holder.item.user.btnFollow.context,
+                        R.drawable.follow_button_background
+                    )
+                }
+
+            holder.item.btnFollow.setOnClickListener {
+                speakerFollowed.value=searchResult[position].is_speaker_followed
+                speakerFollowed.value?.let {
+                    GlobalScope.launch {
+                        if (it.not()) {
+                            try {
+                                val followRequest =
+                                    FollowRequest(searchResult[position].user_id, User.getInstance().userId)
+                                val response =
+                                    RetrofitInstance.profileNetworkService.updateFollowStatus(followRequest)
+                                if (response.isSuccessful) {
+                                    speakerFollowed.value = true
+                                }
+                            }catch (ex:Exception){
+
+                            }
+
+                        } else {
+                            GlobalScope.launch {
+                                try {
+                                    val followRequest =
+                                        FollowRequest(
+                                            searchResult[position].user_id,
+                                            User.getInstance().userId
+                                        )
+                                    val response = RetrofitInstance.profileNetworkService.updateUnfollowStatus(followRequest)
+                                    if (response.isSuccessful) {
+                                        speakerFollowed.value = false
+                                    }
+                                } catch (ex: Exception) {
+
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
 
-        
     }
 }
--- badebhaiya/src/main/java/com/joshtalks/badebhaiya/SearchFragment.kt
+++ badebhaiya/src/main/java/com/joshtalks/badebhaiya/SearchFragment.kt
@@ -1,45 +1,24 @@
 package com.joshtalks.badebhaiya
 
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
-import android.view.KeyEvent
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.EditorInfo
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.content.res.AppCompatResources
-import androidx.core.widget.addTextChangedListener
-import androidx.core.widget.doAfterTextChanged
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.joshtalks.badebhaiya.core.EMPTY
-import com.joshtalks.badebhaiya.core.showToast
-import com.joshtalks.badebhaiya.feed.FeedActivity
-import com.joshtalks.badebhaiya.signup.SignUpActivity
-import kotlinx.android.synthetic.main.activity_sign_up.*
-import kotlinx.android.synthetic.main.fragment_search.*
 import com.joshtalks.badebhaiya.databinding.FragmentSearchBinding
 import com.joshtalks.badebhaiya.feed.FeedViewModel
 import com.joshtalks.badebhaiya.feed.model.Users
-import com.joshtalks.badebhaiya.liveroom.ConversationLiveRoomActivity
-import com.joshtalks.badebhaiya.liveroom.OPEN_PROFILE
-import com.joshtalks.badebhaiya.liveroom.OPEN_ROOM
-import com.joshtalks.badebhaiya.profile.ProfileActivity
 import com.joshtalks.badebhaiya.profile.ProfileViewModel
-import com.joshtalks.badebhaiya.repository.model.ConversationRoomResponse
-import com.joshtalks.badebhaiya.repository.model.User
-import com.joshtalks.badebhaiya.utils.TAG
-import com.joshtalks.badebhaiya.utils.setTextWatcher
-import com.joshtalks.badebhaiya.utils.setUserImageOrInitials
 import kotlinx.android.synthetic.main.activity_feed.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import kotlinx.android.synthetic.main.fragment_search.*
 import kotlinx.android.synthetic.main.li_room_event.view.*
 import kotlinx.android.synthetic.main.li_search_event.*
 import kotlinx.android.synthetic.main.li_search_event.view.*
@@ -57,6 +36,10 @@ class SearchFragment : Fragment() {
 
     val viewModel by lazy {
         ViewModelProvider(requireActivity()).get(FeedViewModel::class.java)
+    }
+
+    private val profileViewModel by lazy {
+        ViewModelProvider(this)[ProfileViewModel::class.java]
     }
 
     lateinit var binding:FragmentSearchBinding
@@ -89,18 +72,21 @@ class SearchFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         var job: Job? = null
         var users=mutableListOf<Users>()
-        binding.searchBar.addTextChangedListener{
-            //var job: Job? = null
 
-                job?.cancel()
-                job = MainScope().launch {
-                    delay(500)
-                    if (it.toString().isNotEmpty())
-                        users=viewModel.searchUser(it.toString())
-                }
-            binding.recyclerView.adapter=SearchAdapter(users)
-            binding.recyclerView.layoutManager=LinearLayoutManager(requireContext())
-        }
+        search()
+
+//        binding.searchBar.addTextChangedListener{
+//            //var job: Job? = null
+//
+//                job?.cancel()
+//                job = MainScope().launch {
+//                    delay(500)
+//                    if (it.toString().isNotEmpty())
+//                        users=viewModel.searchUser(it.toString())
+//                }
+//            binding.recyclerView.adapter=SearchAdapter(users)
+//            binding.recyclerView.layoutManager=LinearLayoutManager(requireContext())
+//        }
 
 //        binding.searchBar.addTextChangedListener( object: TextWatcher {
 //            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
@@ -121,6 +107,28 @@ class SearchFragment : Fragment() {
 //        })
     }
 
+
+    fun search(){
+        binding.searchBar.addTextChangedListener( object: TextWatcher {
+            var job: Job? = null
+            var users=mutableListOf<Users>()
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                job?.cancel()
+                job = MainScope().launch {
+                    delay(500)
+                    if (binding.searchBar.toString().isNotEmpty())
+                        users=viewModel.searchUser(binding.searchBar.text.toString())
+                    //showToast("${binding.searchBar.text}")
+                }
+            }
+            override fun afterTextChanged(p0: Editable?) {
+                binding.recyclerView.adapter=SearchAdapter(users)
+                binding.recyclerView.layoutManager=LinearLayoutManager(requireContext())
+            }
+        })
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
