417f09b92a71256aecda544c71a48cae608c1b89
Miihir79 <mihirrshah02@gmail.com>
2022-05-19 22:51:55 +0530

    UI gridview done

--- app/src/main/java/com/joshtalks/joshskills/ui/cohort_based_course/utils/CbcBindingAdapter.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/cohort_based_course/utils/CbcBindingAdapter.kt
@@ -1,9 +1,11 @@
 package com.joshtalks.joshskills.ui.cohort_based_course.utils
 
+import android.graphics.drawable.Drawable
 import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import androidx.databinding.BindingAdapter
+import com.google.android.material.button.MaterialButton
 import com.joshtalks.joshskills.R
 import com.joshtalks.joshskills.core.TAG
 
@@ -13,4 +15,16 @@ fun AutoCompleteTextView.setTextAdapter(a:String) {
     val option = resources.getStringArray(R.array.form_options)
     val arrayAdapter = ArrayAdapter(context, R.layout.dropdown_item, option)
     this.setAdapter(arrayAdapter)
+}
+
+@BindingAdapter("setButtonBackground")
+fun MaterialButton.setBackgroundState(boolean: Boolean){
+    when(boolean){
+        true->{
+            this.isEnabled = true
+        }
+        false->{
+            this.isEnabled = false
+        }
+    }
 }
--- app/src/main/java/com/joshtalks/joshskills/ui/cohort_based_course/views/ScheduleFragment.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/cohort_based_course/views/ScheduleFragment.kt
@@ -1,12 +1,12 @@
 package com.joshtalks.joshskills.ui.cohort_based_course.views
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.GridLayoutManager
 import com.joshtalks.joshskills.R
 import com.joshtalks.joshskills.base.BaseFragment
 import com.joshtalks.joshskills.databinding.FragmentScheduleBinding
@@ -25,7 +25,15 @@ class ScheduleFragment: BaseFragment() {
         binding.executePendingBindings()
     }
 
-    override fun initViewState() {}
+    override fun initViewState() {
+        Log.d("TAG", "initViewState: reached")
+        val timeList = listOf("10:00 PM - 11:00 PM","12:00 PM - 01:00 PM", // demo time slots
+            "02:00 PM - 03:00 PM","04:00 PM - 05:00 PM","06:00 PM - 07:00 PM","08:00 PM - 09:00 PM")
+
+        binding.recyclerView2.apply {
+            adapter = ScheduleAdapter(timeList)
+        }
+    }
 
     override fun setArguments() {}
 
@@ -35,14 +43,10 @@ class ScheduleFragment: BaseFragment() {
     ): View {
         binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_schedule, container, false)
-
-        val timeList = listOf("10:00 PM - 11:00 PM","12:00 PM - 01:00 PM", // demo time slots
-            "02:00 PM - 03:00 PM","04:00 PM - 05:00 PM","06:00 PM - 07:00 PM","08:00 PM - 09:00 PM")
-        val adapter = ScheduleAdapter(timeList)
-        binding.recyclerView2.adapter = adapter
-        binding.recyclerView2.layoutManager = GridLayoutManager(context,2)
-
         return binding.root
+
+
+
     }
 
 }
