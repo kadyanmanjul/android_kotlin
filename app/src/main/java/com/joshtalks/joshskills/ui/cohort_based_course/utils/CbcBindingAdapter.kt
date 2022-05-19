e5426055698d7b21965c3eadda17acf7d1898572
namanmahendra01 <namanmahendra01@gmail.com>
2022-05-19 18:36:32 +0530

    PromiseFragment Adaper

---
+++ app/src/main/java/com/joshtalks/joshskills/ui/cohort_based_course/utils/CbcBindingAdapter.kt
@@ -0,0 +1,16 @@
+package com.joshtalks.joshskills.ui.cohort_based_course.utils
+
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.databinding.BindingAdapter
+import com.joshtalks.joshskills.R
+import com.joshtalks.joshskills.core.TAG
+
+@BindingAdapter("setTextAdapter")
+fun AutoCompleteTextView.setTextAdapter(a:String) {
+    Log.d(TAG, "setTextAdapter: 1")
+    val option = resources.getStringArray(R.array.form_options)
+    val arrayAdapter = ArrayAdapter(context, R.layout.dropdown_item, option)
+    this.setAdapter(arrayAdapter)
+}
