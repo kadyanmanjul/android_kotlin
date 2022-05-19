0ba367579a9919d011bff4b295be4190324a849d
namanmahendra01 <namanmahendra01@gmail.com>
2022-05-20 02:01:53 +0530

    Permission added on Incoming Call of New P2p

--- app/src/main/java/com/joshtalks/joshskills/ui/voip/new_arch/ui/views/VoiceCallActivity.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/voip/new_arch/ui/views/VoiceCallActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.commit
 import androidx.lifecycle.ViewModelProvider
+import com.afollestad.materialdialogs.MaterialDialog
 import com.joshtalks.joshskills.R
 import com.joshtalks.joshskills.base.BaseActivity
 import com.joshtalks.joshskills.base.constants.FROM_ACTIVITY
@@ -13,6 +14,7 @@ import com.joshtalks.joshskills.base.constants.FROM_INCOMING_CALL
 import com.joshtalks.joshskills.base.constants.INTENT_DATA_COURSE_ID
 import com.joshtalks.joshskills.base.constants.INTENT_DATA_INCOMING_CALL_ID
 import com.joshtalks.joshskills.base.constants.INTENT_DATA_TOPIC_ID
+import com.joshtalks.joshskills.core.PermissionUtils
 import com.joshtalks.joshskills.databinding.ActivityVoiceCallBinding
 import com.joshtalks.joshskills.ui.voip.new_arch.ui.viewmodels.VoiceCallViewModel
 import com.joshtalks.joshskills.ui.voip.new_arch.ui.viewmodels.voipLog
@@ -22,6 +24,10 @@ import com.joshtalks.joshskills.voip.data.local.PrefManager
 import com.joshtalks.joshskills.voip.voipanalytics.CallAnalytics
 import com.joshtalks.joshskills.voip.voipanalytics.EventName
 import kotlinx.coroutines.sync.Mutex
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 private const val TAG = "VoiceCallActivity"
 
@@ -84,12 +90,48 @@ class VoiceCallActivity : BaseActivity() {
 
     override fun onCreated() {
         Log.d(TAG, "onCreated: ${vm.source}")
-        if (vm.source == FROM_INCOMING_CALL || vm.source == FROM_CALL_BAR) {
-            addCallUserFragment()
-        } else if (vm.source == FROM_ACTIVITY) {
-            addSearchingUserFragment()
+        if (PermissionUtils.isCallingPermissionEnabled(this)) {
+            if (vm.source == FROM_INCOMING_CALL || vm.source == FROM_CALL_BAR) {
+                addCallUserFragment()
+            } else if (vm.source == FROM_ACTIVITY) {
+                addSearchingUserFragment()
+            }
+        }else{
+            getPermission()
         }
     }
+
+    private fun getPermission() {
+        PermissionUtils.callingFeaturePermission(
+            this,
+            object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    report?.areAllPermissionsGranted()?.let { flag ->
+                        if (report.isAnyPermissionPermanentlyDenied) {
+                            PermissionUtils.callingPermissionPermanentlyDeniedDialog(
+                                this@VoiceCallActivity,
+                                message = R.string.call_start_permission_message
+                            )
+                            return
+                        }
+                        if (flag) {
+                            addCallUserFragment()
+                            return
+                        } else {
+                            finish()
+                        }
+                    }
+                }
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    token?.continuePermissionRequest()
+                }
+            }
+        )
+    }
+
 
     override fun initViewState() {
         event.observe(this) {
