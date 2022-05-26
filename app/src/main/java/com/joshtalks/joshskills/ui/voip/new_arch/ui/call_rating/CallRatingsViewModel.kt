04282abf974c78b797454093a2b55b47783a4b23
namanmahendra01 <namanmahendra01@gmail.com>
2022-05-26 13:23:01 +0530

    Call Rating Duration Text Fix

--- app/src/main/java/com/joshtalks/joshskills/ui/voip/new_arch/ui/call_rating/CallRatingsViewModel.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/voip/new_arch/ui/call_rating/CallRatingsViewModel.kt
@@ -2,6 +2,7 @@ package com.joshtalks.joshskills.ui.voip.new_arch.ui.call_rating
 
 import androidx.lifecycle.viewModelScope
 import com.joshtalks.joshskills.base.BaseViewModel
+import com.joshtalks.joshskills.ui.call.data.local.VoipPref
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -17,6 +18,43 @@ class CallRatingsViewModel: BaseViewModel() {
                 ex.printStackTrace()
             }
         }
+    }
+    fun getCallDurationString() : String{
+        val mTime = StringBuilder()
+        var second = VoipPref.getLastCallDurationInSec()
+        val minute = getDurationInMin()
+        if (minute > 0) {
+            mTime.append(minute).append(getMinuteString(minute))
+        }
+        if (second > 0) {
+            if(second<60){
+                mTime.append(second).append(getSecondString(second))
+            }else{
+                second %= 60
+                mTime.append(second).append(getSecondString(second))
+            }
+        }
+        return mTime.toString()
+    }
+
+    fun getDurationInMin(): Int {
+        val second = VoipPref.getLastCallDurationInSec()
+        val min = (second % 3600) / 60
+        return min.toInt()
+    }
+
+    private fun getMinuteString(min: Int): String {
+        if (min > 1) {
+            return " minutes "
+        }
+        return " minute "
+    }
+
+    private fun getSecondString(sec: Long): String {
+        if (sec > 1L) {
+            return " seconds "
+        }
+        return " second "
     }
 
     fun submitCallRatings(agoraCallId : String, rating : Int, callerMentorId : String) {
