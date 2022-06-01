814d4227dc96f84479e5fc80bf0387fc7268ff95
saisukesh04 <sukesh.sai04@gmail.com>
2022-06-01 16:37:30 +0530

    Removed Emoji files and folders

--- emojiBase/src/main/java/com/vanniktech/emoji/listeners/OnEmojiClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.vanniktech.emoji.listeners;
-
-import androidx.annotation.NonNull;
-import com.vanniktech.emoji.EmojiImageView;
-import com.vanniktech.emoji.emoji.Emoji;
-
-public interface OnEmojiClickListener {
-  void onEmojiClick(@NonNull EmojiImageView emoji, @NonNull Emoji imageView);
-}
