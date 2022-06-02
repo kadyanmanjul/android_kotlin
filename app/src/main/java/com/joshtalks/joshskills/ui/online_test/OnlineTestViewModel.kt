062dd21deaca23e34d7529d0e0b5d9597a2f0005
yashkasera <yashkasera@icloud.com>
2022-06-02 16:16:29 +0530

    Bug Fixes

--- app/src/main/java/com/joshtalks/joshskills/ui/online_test/OnlineTestViewModel.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/online_test/OnlineTestViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.joshtalks.joshskills.core.*
+import com.joshtalks.joshskills.core.abTest.repository.ABTestRepository
 import com.joshtalks.joshskills.core.analytics.MixPanelEvent
 import com.joshtalks.joshskills.core.analytics.MixPanelTracker
 import com.joshtalks.joshskills.core.analytics.ParamKeys
@@ -16,11 +17,7 @@ import com.joshtalks.joshskills.repository.local.model.Mentor
 import com.joshtalks.joshskills.repository.local.model.assessment.Assessment
 import com.joshtalks.joshskills.repository.local.model.assessment.AssessmentQuestionWithRelations
 import com.joshtalks.joshskills.repository.local.model.assessment.Choice
-import com.joshtalks.joshskills.repository.server.assessment.AssessmentStatus
-import com.joshtalks.joshskills.repository.server.assessment.AssessmentType
-import com.joshtalks.joshskills.repository.server.assessment.OnlineTestRequest
-import com.joshtalks.joshskills.repository.server.assessment.OnlineTestResponse
-import com.joshtalks.joshskills.core.abTest.repository.ABTestRepository
+import com.joshtalks.joshskills.repository.server.assessment.*
 import com.joshtalks.joshskills.util.showAppropriateMsg
 import com.tonyodev.fetch2.NetworkType
 import com.tonyodev.fetch2.Priority
@@ -49,6 +46,7 @@ class OnlineTestViewModel(application: Application) : AndroidViewModel(applicati
                     response.body()?.let {
                         if (it.totalQuestions == it.totalAnswered)
                             it.totalAnswered = 0
+                        it.question?.choices?.let { choices -> randomizeChoices(choices) }
                         grammarAssessmentLiveData.postValue(it)
                     }
                 }
@@ -69,6 +67,11 @@ class OnlineTestViewModel(application: Application) : AndroidViewModel(applicati
                     if (response.isSuccessful) {
                         apiStatus.postValue(ApiCallStatus.SUCCESS)
                         response.body()?.let { onlineTestResponse ->
+                            onlineTestResponse.question?.choices?.let { choices ->
+                                randomizeChoices(
+                                    choices
+                                )
+                            }
                             grammarAssessmentLiveData.postValue(onlineTestResponse)
                         }
                     }
@@ -82,6 +85,14 @@ class OnlineTestViewModel(application: Application) : AndroidViewModel(applicati
                 ex.showAppropriateMsg()
             }
         }
+    }
+
+    fun randomizeChoices(choiceList: List<ChoiceResponse>) {
+        val shuffledList = choiceList.shuffled()
+        shuffledList.forEachIndexed { index, choiceResponse ->
+            choiceResponse.sortOrder = index
+        }
+        grammarAssessmentLiveData.value?.question?.choices = shuffledList
     }
 
     fun fetchQuestionsOrPostAnswer(lessonId: Int) {
