a20fa63f025fc62f56350e6f3e8e77836626ec7b
Sagar Jain <sagar@joshtalks.com>
2022-06-16 21:12:14 +0530

    Notification call vie worker done

--- app/src/main/java/com/joshtalks/joshskills/core/service/Workers.kt
+++ app/src/main/java/com/joshtalks/joshskills/core/service/Workers.kt
@@ -85,10 +85,14 @@ import com.joshtalks.joshskills.repository.server.MessageStatusRequest
 import com.joshtalks.joshskills.repository.server.UpdateDeviceRequest
 import com.joshtalks.joshskills.repository.server.onboarding.VersionResponse
 import com.joshtalks.joshskills.track.CourseUsageSync
+import com.joshtalks.joshskills.ui.inbox.InboxActivity
 import com.joshtalks.joshskills.ui.launch.LauncherActivity
 import com.joshtalks.joshskills.ui.payment.FreeTrialPaymentActivity
 import com.joshtalks.joshskills.ui.payment.order_summary.PaymentSummaryActivity
+import com.joshtalks.joshskills.ui.video_player.VideoPlayerActivity
+import com.joshtalks.joshskills.ui.voip.NotificationId
 import com.joshtalks.joshskills.ui.voip.NotificationId.Companion.LOCAL_NOTIFICATION_CHANNEL
+import com.joshtalks.joshskills.ui.voip.new_arch.ui.call_recording.ProcessCallRecordingService
 import com.yariksoffice.lingver.Lingver
 import io.branch.referral.Branch
 import java.util.Date
@@ -515,6 +519,106 @@ class DeterminedNPSEvent(context: Context, private var workerParams: WorkerParam
         return Result.success()
     }
 }
+
+class CallRecordingEvent(context: Context, private var workerParams: WorkerParameters) :
+    CoroutineWorker(context, workerParams) {
+    override suspend fun doWork(): Result {
+        try {
+            val filePath = workerParams.inputData.getString("filepath") ?: EMPTY
+            val context = AppObjectController.joshApplication
+            val textDescription = "Well done!, Here is your call recording"
+            val title = "Processed Call Recording"
+            val index = ProcessCallRecordingService.LOCAL_NOTIFICATION_ID
+            val intent = VideoPlayerActivity.getActivityIntent(
+                context = context,
+                videoTitle = "Call Recorded",
+                videoId = null,
+                videoUrl = filePath
+            ).apply {
+                addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+            }
+
+            intent.run {
+                val activityList = arrayOf(Intent(applicationContext, InboxActivity::class.java), this)
+                val uniqueInt = (System.currentTimeMillis() and 0xfffffff).plus(index).toInt()
+                val defaultSound =
+                    RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+                val pendingIntent = PendingIntent.getActivities(
+                    context,
+                    uniqueInt,
+                    activityList,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+
+                val style = NotificationCompat.BigTextStyle()
+                style.setBigContentTitle(title)
+                style.bigText(textDescription)
+                style.setSummaryText("")
+
+                val notificationBuilder =
+                    NotificationCompat.Builder(
+                        context,
+                        NotificationId.LOCAL_NOTIFICATION_CHANNEL + index
+                    )
+                        .setSmallIcon(R.drawable.ic_status_bar_notification)
+                        .setContentTitle(title)
+                        .setAutoCancel(true)
+                        .setSound(defaultSound)
+                        .setContentText(textDescription)
+                        .setContentIntent(pendingIntent)
+                        .setStyle(style)
+                        .setWhen(System.currentTimeMillis())
+                        .setDefaults(Notification.DEFAULT_ALL)
+                        .setColor(
+                            ContextCompat.getColor(
+                                context,
+                                R.color.colorAccent
+                            )
+                        )
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    notificationBuilder.priority = NotificationManager.IMPORTANCE_DEFAULT
+                }
+
+                val dismissIntent =
+                    Intent(
+                        context.applicationContext,
+                        LocalNotificationDismissEventReceiver::class.java
+                    )
+                val dismissPendingIntent: PendingIntent =
+                    PendingIntent.getBroadcast(
+                        context.applicationContext,
+                        uniqueInt,
+                        dismissIntent,
+                        0
+                    )
+
+                notificationBuilder.setDeleteIntent(dismissPendingIntent)
+
+                val notificationManager =
+                    context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    val notificationChannel = NotificationChannel(
+                        NotificationId.LOCAL_NOTIFICATION_CHANNEL + index,
+                        NotificationId.LOCAL_NOTIFICATION_CHANNEL + index,
+                        NotificationManager.IMPORTANCE_HIGH
+                    )
+                    notificationChannel.enableLights(true)
+                    notificationChannel.enableVibration(true)
+                    notificationBuilder.setChannelId(NotificationId.LOCAL_NOTIFICATION_CHANNEL + index)
+                    notificationManager.createNotificationChannel(notificationChannel)
+                }
+                notificationManager.notify(uniqueInt, notificationBuilder.build())
+            }
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
+        return Result.success()
+    }
+}
+
 
 class UpdateDeviceDetailsWorker(context: Context, workerParams: WorkerParameters) :
     CoroutineWorker(context, workerParams) {
@@ -1051,7 +1155,6 @@ class LocalNotificationWorker(
     }
 
     private fun checkOnBoardingStage(context: Context) {
-        showOnBoardingCompletionNotification(context)
         val onBoardingStage = PrefManager.getStringValue(ONBOARDING_STAGE)
         val isOnBoardingUnfinished = onBoardingStage == OnBoardingStage.APP_INSTALLED.value ||
                 onBoardingStage == OnBoardingStage.START_NOW_CLICKED.value ||
@@ -1067,7 +1170,6 @@ class LocalNotificationWorker(
             contentText = "Fulfilling your dream of speaking in English"
             action = NotificationAction.ACTION_COMPLETE_ONBOARDING
         }
-        Log.d("sagar", "showOnBoardingCompletionNotification() called with: context = $context")
         NotificationUtils(context).sendNotification(nc)
     }
 }
