2ebcba6979bf04a61f72767af6f9494e4918a650
Sagar Jain <sagar@joshtalks.com>
2022-06-20 14:44:17 +0530

    Ui fixes

--- app/src/main/java/com/joshtalks/joshskills/core/notification/NotificationUtils.kt
+++ app/src/main/java/com/joshtalks/joshskills/core/notification/NotificationUtils.kt
@@ -9,6 +9,7 @@ import android.content.Intent
 import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
