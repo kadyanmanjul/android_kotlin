e583767583ac68a1bfef9fc8f63e470b297e1c75
sahilkhan1 <sahilkhan@joshtalks.com>
2022-06-24 13:39:17 +0530

    bugfix: increased read timeout to fix crash read timeout exception

--- badebhaiya/src/main/java/com/joshtalks/badebhaiya/repository/service/RetrofitInstance.kt
+++ badebhaiya/src/main/java/com/joshtalks/badebhaiya/repository/service/RetrofitInstance.kt
@@ -26,7 +26,7 @@ import java.util.concurrent.TimeUnit
 import timber.log.Timber
 
 const val KEY_AUTHORIZATION = "Authorization"
-private const val READ_TIMEOUT = 30L
+private const val READ_TIMEOUT = 50L
 private const val WRITE_TIMEOUT = 30L
 private const val CONNECTION_TIMEOUT = 30L
 private const val CALL_TIMEOUT = 60L
