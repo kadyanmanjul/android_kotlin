b021e333c1609d4d4f322a127094135070d2c47a
saisukesh04 <sukesh.sai04@gmail.com>
2022-06-27 17:33:38 +0530

    Notification analytics API changes

--- app/src/main/java/com/joshtalks/joshskills/core/firestore/NotificationAnalyticsRequest.kt
+++ app/src/main/java/com/joshtalks/joshskills/core/firestore/NotificationAnalyticsRequest.kt
@@ -1,8 +1,13 @@
 package com.joshtalks.joshskills.core.firestore
 
+import com.google.gson.annotations.SerializedName
+import com.joshtalks.joshskills.repository.local.model.Mentor
+
 data class NotificationAnalyticsRequest(
     val id: String,
     val timestamp: Long,
     val action : String,
-    val channel :String?
+    val channel :String?,
+    @SerializedName("mentor_id")
+    val mentorId: String = Mentor.getInstance().getId()
 )
--- app/src/main/java/com/joshtalks/joshskills/repository/service/UtilsAPIService.kt
+++ app/src/main/java/com/joshtalks/joshskills/repository/service/UtilsAPIService.kt
@@ -6,6 +6,7 @@ import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 @JvmSuppressWildcards
 interface UtilsAPIService {
@@ -14,7 +15,7 @@ interface UtilsAPIService {
     suspend fun getServerTime(): Response<Long>
 
     @GET("$DIR/notification/missed_notification/")
-    suspend fun getMissedNotifications(): Response<List<MissedNotification>>
+    suspend fun getMissedNotifications(@Query("mentor_id") mentorId: String): Response<List<MissedNotification>>
 
     @POST("$DIR/notification/analytics_v2/")
     suspend fun engageNewNotificationAsync(@Body params: NotificationAnalyticsRequest): Map<String, String>
