1f1ca4ae27e97bacf43ce5ea7924ad8ba22650eb
Miihir79 <mihirrshah02@gmail.com>
2022-06-28 18:32:36 +0530

    form changes made

--- app/src/main/java/com/joshtalks/joshskills/ui/certification_exam/report/udetail/CertificateDetailActivity.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/certification_exam/report/udetail/CertificateDetailActivity.kt
@@ -371,6 +371,10 @@ class CertificateDetailActivity : BaseActivity(), FileDownloadCallback {
                 showToast(getString(R.string.enter_postal_town))
                 return@launch
             }
+            runOnUiThread {
+                showProgressDialog(getString(R.string.generating_certificate))
+            }
+
             if (isInternetAvailable()) {
                 viewModel.postCertificateUserDetails(getUserDetail())
                 viewModel.saveImpression(GENERATE_CERTIFICATE_FORM)
