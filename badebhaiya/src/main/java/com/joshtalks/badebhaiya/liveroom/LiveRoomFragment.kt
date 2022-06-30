2fe02007cd456760c977f943baf9e74767e29003
sahilkhan1 <sahilkhan@joshtalks.com>
2022-06-30 16:29:36 +0530

    feat: eliminated user consent on move to speaker and showing notification on move to audience and speaker

--- badebhaiya/src/main/java/com/joshtalks/badebhaiya/liveroom/LiveRoomFragment.kt
+++ badebhaiya/src/main/java/com/joshtalks/badebhaiya/liveroom/LiveRoomFragment.kt
@@ -268,13 +268,19 @@ class LiveRoomFragment : BaseFragment<FragmentLiveRoomBinding, LiveRoomViewModel
 
                 }
                 SHOW_NOTIFICATION_FOR_USER_TO_JOIN -> {
-                    setNotificationBarFieldsWithActions(
-                        "Maybe later?", "Join as speaker", String.format(
-                            "\uD83D\uDC4B %s invited you to join as a speaker",
-                            PubNubManager.moderatorName
-                        ), PubNubManager.moderatorUid,
-                        NotificationView.ConversationRoomNotificationState.JOIN_AS_SPEAKER
+                    PubNubEventsManager.moveToSpeakerEvent()
+                    isInviteRequestComeFromModerator = true
+                    setNotificationWithoutAction(
+                        "You have joined as a speaker", true,
+                        NotificationView.ConversationRoomNotificationState.DEFAULT
                     )
+//                    setNotificationBarFieldsWithActions(
+//                        "Maybe later?", "Join as speaker", String.format(
+//                            "\uD83D\uDC4B %s invited you to join as a speaker",
+//                            PubNubManager.moderatorName
+//                        ), PubNubManager.moderatorUid,
+//                        NotificationView.ConversationRoomNotificationState.JOIN_AS_SPEAKER
+//                    )
                 }
                 CHANGE_MIC_STATUS -> {
                     it.data?.let {
@@ -318,6 +324,10 @@ class LiveRoomFragment : BaseFragment<FragmentLiveRoomBinding, LiveRoomViewModel
                     }
                 }
                 MOVE_TO_AUDIENCE -> {
+                    setNotificationWithoutAction(
+                        "You have been moved back to audience", true,
+                        NotificationView.ConversationRoomNotificationState.DEFAULT
+                    )
                     it.data?.let {
                         val user = it.getParcelable<LiveRoomUser>(NOTIFICATION_USER)
                         if (PubNubManager.getLiveRoomProperties()?.agoraUid == user?.id) {
