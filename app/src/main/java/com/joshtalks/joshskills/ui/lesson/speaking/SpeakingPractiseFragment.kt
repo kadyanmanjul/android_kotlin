7ada8fb24d9fc470d10196eacd4018cf3a9cec18
saisukesh04 <sukesh.sai04@gmail.com>
2022-06-30 11:15:17 +0530

    Recent call for other languages fixed

--- app/src/main/java/com/joshtalks/joshskills/ui/lesson/speaking/SpeakingPractiseFragment.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/lesson/speaking/SpeakingPractiseFragment.kt
@@ -152,7 +152,7 @@ class SpeakingPractiseFragment : CoreJoshFragment() {
         binding.vm = viewModel
         binding.rootView.layoutTransition?.setAnimateParentHierarchy(false)
         binding.markAsCorrect.isVisible = BuildConfig.DEBUG
-        if (PrefManager.getStringValue(CURRENT_COURSE_ID) == PrefManager.getStringValue(DEFAULT_COURSE_ID))
+        if (PrefManager.getStringValue(CURRENT_COURSE_ID) == DEFAULT_COURSE_ID)
             binding.imgRecentCallsHistory.visibility = VISIBLE
         // showTooltip()
         return binding.rootView
