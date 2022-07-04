0cd9cc85479fab85e8878f3bd67bc7df9e1f4c9f
Miihir79 <mihirrshah02@gmail.com>
2022-07-04 14:50:40 +0530

    answer list scrolling issue fixed

---
+++ app/src/main/java/com/joshtalks/joshskills/core/custom_ui/decorator/GridSpacingQuestionsDecoration.kt
@@ -0,0 +1,17 @@
+package com.joshtalks.joshskills.core.custom_ui.decorator
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class GridSpacingQuestionsDecoration( var spanCount: Int,  var spacing: Int, var includeEdge: Boolean) :
+    RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        super.getItemOffsets(outRect, view, parent, state)
+        val position = parent.getChildAdapterPosition(view)
+        outRect.top = spacing
+        outRect.bottom = spacing
+        outRect.right = spacing
+    }
+}
