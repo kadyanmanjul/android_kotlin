839c7e84a5ffc7399ba21dc6bb7298b3765e921a
sahilkhan1 <sahilkhan@joshtalks.com>
2022-08-23 12:36:53 +0530

    feat(call_with_expert): created call with expert activity

---
+++ app/src/main/java/com/joshtalks/joshskills/ui/callWithExpert/CallWithExpertActivity.kt
@@ -0,0 +1,12 @@
+package com.joshtalks.joshskills.ui.callWithExpert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.joshtalks.joshskills.R
+
+class CallWithExpertActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_call_with_expert)
+    }
+}
