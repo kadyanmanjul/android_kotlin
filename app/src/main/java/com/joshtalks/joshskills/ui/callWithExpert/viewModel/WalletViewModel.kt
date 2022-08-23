e4f40f525e6510281ad7c027c5ecd0b336cee0cc
sahilkhan1 <sahilkhan@joshtalks.com>
2022-08-23 15:42:55 +0530

    feat(call_with_expert): created credit wallet fragment

--- app/src/main/java/com/joshtalks/joshskills/ui/callWithExpert/viewModel/WalletViewModel.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/callWithExpert/viewModel/WalletViewModel.kt
@@ -1,14 +1,24 @@
 package com.joshtalks.joshskills.ui.callWithExpert.viewModel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.joshtalks.joshskills.R
 
-class WalletViewModel : ViewModel() {
+class WalletViewModel(application: Application) : AndroidViewModel(application) {
 
     private val _availableBalance = MutableLiveData<String>()
 
     val availableBalance: LiveData<String>
         get() = _availableBalance
+
+    private val _addedAmount = MutableLiveData(application.getString(R.string.enter_amount_in_inr))
+
+    val addedAmount: LiveData<String>
+        get() = _addedAmount
+
+
 
 }
