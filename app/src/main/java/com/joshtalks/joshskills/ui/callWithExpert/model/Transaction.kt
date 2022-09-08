7a24d11314a7c9367a52192c89ed954a488b993a
Miihir79 <mihirrshah02@gmail.com>
2022-09-08 17:56:39 +0530

    null handled

--- app/src/main/java/com/joshtalks/joshskills/ui/callWithExpert/model/Transaction.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/callWithExpert/model/Transaction.kt
@@ -3,6 +3,6 @@ package com.joshtalks.joshskills.ui.callWithExpert.model
 data class Transaction(
     val amount: Int,
     val created: Long,
-    val event_log: String,
+    val event_log: String?,
     val is_failed: Boolean
 )
--- app/src/main/java/com/joshtalks/joshskills/ui/callWithExpert/model/WalletLogs.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/callWithExpert/model/WalletLogs.kt
@@ -3,7 +3,7 @@ package com.joshtalks.joshskills.ui.callWithExpert.model
 data class WalletLogs (
     val amount: Int,
     val created: Long,
-    val event_log: String,
+    val event_log: String?,
     val is_failed: Boolean,
     val payment_id: String
 )
