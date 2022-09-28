86d77310e9cfd7807ff834590a694ca4941ddb3b
sahilkhan1 <sahilkhan@joshtalks.com>
2022-09-28 16:31:25 +0530

    fixed loading hiding before the time

--- app/src/main/java/com/joshtalks/joshskills/ui/paymentManager/PaymentGatewayManager.kt
+++ app/src/main/java/com/joshtalks/joshskills/ui/paymentManager/PaymentGatewayManager.kt
@@ -140,7 +140,6 @@ class PaymentGatewayManager(
             joshTalksId = orderDetails.joshtalksOrderId
             amount = orderDetails.amount
 //            paymentGatewayListener?.onWarmUpEnded()
-            paymentGatewayListener?.onWarmUpEnded()
             hyperInstance.process(payload)
         } catch (e: Exception) {
             Log.e("sagar", "initializJuspayPayment:2 ${e.message}")
