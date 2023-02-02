f14ade9baae484d1599b57b47ea49680c32fd368
Manjul Kadyan <manjulkadyan@joshtalks.com>
2023-02-02 10:14:16 +0530

    Reduce app size, delete obsolete code and unused dependencies

--- app/src/main/java/com/joshtalks/joshskills/repository/local/AppDatabase.kt
+++ app/src/main/java/com/joshtalks/joshskills/repository/local/AppDatabase.kt
@@ -746,6 +746,7 @@ abstract class AppDatabase : RoomDatabase() {
                 database.execSQL("ALTER TABLE `buy_course_feature` ADD COLUMN `slider_image` TEXT NOT NULL DEFAULT ''")
                 database.execSQL("ALTER TABLE `buy_course_feature` ADD COLUMN `testimonials_video` TEXT NOT NULL DEFAULT ''")
                 database.execSQL("ALTER TABLE `buy_course_feature` ADD COLUMN `live_messages` TEXT NOT NULL DEFAULT ''")
+                database.execSQL("DROP TABLE `special_table`")
             }
         }
 
