bcd8e9db84c31ab064a1be53da3988f4e9d150dc
Bhaskar Singh <bhaskarsingh@joshtalks.com>
2023-03-09 18:09:40 +0530

    WIP
    Signed-off-by: Bhaskar Singh <bhaskarsingh@joshtalks.com>

--- app/src/main/java/com/joshtalks/joshskills/util/InstantUtils.kt
+++ app/src/main/java/com/joshtalks/joshskills/util/InstantUtils.kt
@@ -3,18 +3,23 @@ package com.joshtalks.joshskills.util
 import android.content.Context
 import android.os.Build
 import androidx.annotation.RequiresApi
+import com.google.android.gms.instantapps.InstantApps
 
 /**
  * Reads the Instant App Cookie
  */
 @RequiresApi(Build.VERSION_CODES.O)
-fun Context.readCookie() = packageManager.instantAppCookie.toString(Charsets.UTF_8)
+fun Context.readCookie() : String {
+    val packageManager = InstantApps.getPackageManagerCompat(applicationContext)
+    return packageManager.instantAppCookie?.toString(Charsets.UTF_8) ?: "Instant App Cookie is NULL"
+}
 
 /**
  * Clear the Instant App Cookie
  */
 @RequiresApi(Build.VERSION_CODES.O)
 fun Context.clearCookie(): String {
-    packageManager.clearInstantAppCookie()
+    val packageManager = InstantApps.getPackageManagerCompat(applicationContext)
+    packageManager.instantAppCookie = null
     return "Cookie cleared"
 }
